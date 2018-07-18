6e16a4772ce80d421cbca2e65aa4d315071b9a5e
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-07-18 15:26:46 -0700

    Explain what "<>" does in this context

--- purescript/01_Basic/04_Newtypes.purs
+++ purescript/01_Basic/04_Newtypes.purs
@@ -4,7 +4,7 @@ newtype NewTypeName = OnlyConstructor OnlyArgument
 -- For example....
 newtype Miles = Miles Int
 instance showMiles :: Show Miles where
-  show (Miles i) = show i <> " miles"
+  show (Miles i) = show i <> " miles" -- "<>" appends two strings in this context
 
 print :: Miles -> String
 print miles = "You ran " <> show miles <> "."
