f097fd2c4435f0c3cc9d1d9d67dbd723119ce833
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-07-31 11:46:56 -0700

    Demonstrate infix for type aliases, data types, and their constructors

--- purescript/01-Syntax/02-Values-and-Functions/11-Regular-Infix-Notation.purs
+++ purescript/01-Syntax/02-Values-and-Functions/11-Regular-Infix-Notation.purs
@@ -6,16 +6,25 @@ two_arg_function x y | x < 0 = (x + 1) * (y + 14)
 1 `two_arg_function` 2
 -- becomes two_arg_function 1 2
 
-type SomeType = forall a b. List a -> Box a
+-- Some types given here to make things easier...
+type TypeAlias = forall a b. List a -> Box a
+data DataType = Constructor Int Int
 
--- Infix Syntax
-infix/infixl/infixr precedence functionName as functionAlias
-infix/infixl/infixr precedence SomeType as typeAlias
+-- Infix Syntax where 'precedence' is a number (0..9)
+infix/infixl/infixr precedence function/dataType/constructor as alias
+-- or for type aliases:
+infix/infixl/infixr precedence type TypeName as alias
 
--- Infix is all about where to put the parenthesis
--- where precedence is 0 = do first
---                     n = do after first but before last
---                     9 = do last
+-- Example
+infixl 4 two_arg_function as >o_o>
+infixr 4 DataType as :-->
+infix  2 Constructor as ?->
+infix  4 type TypeAlias as :$>
+
+-- Infix is all about where to put the parenthesis as indicated by precedence:
+-- precedence is 0 = group first
+--               n = group after first but before last
+--               9 = group last
 --
 -- Each type of infix will be shown by reducing it to its final call
 
