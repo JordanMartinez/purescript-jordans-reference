69ce1454667d460a083642f2453bd69a4ea3dfa9
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-14 13:49:16 -0700

    Remove underscore-abbreviated function: used in an invalid context

--- 11-Syntax/01-Basic-Syntax/src/02-Values-and-Functions/05-Abbreviated-Functions.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Values-and-Functions/05-Abbreviated-Functions.purs
@@ -2,12 +2,7 @@ module Syntax.Function.BodyAbbreviation where
 
 import Prelude
 
--- the underscore indicates the next argument that the function will receive
-abbreviatedFunction :: Int -> Int
-abbreviatedFunction   = (+) 3  {- is the same as...
-abbreviatedFunction i = 3 + i  -}
-
--- if the body of a function is another function expected an argument,
+-- if the body of a function is another function that expects an argument,
 -- one can omit the argument entirely.
 --    Note: 'show' converts any type into a String
 abbreviatedFunction2 :: Int -> String
@@ -15,8 +10,5 @@ abbreviatedFunction2   = show   {- is the same as...
 abbreviatedFunction2 x = show x -} --
 
 -- example
-exampleAbbreviation1 :: Boolean
-exampleAbbreviation1 = (abbreviatedFunction 4) == 7
-
 exampleAbbreviation2 :: Boolean
 exampleAbbreviation2 = (abbreviatedFunction2 4) == "4"
