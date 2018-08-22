43301fa83a7fb8ebbd25471fe39ef3c8a7d263bc
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-21 20:06:29 -0700

    Explain function currying by removing (->) syntax sugar

--- 11-Syntax/01-Basic-Syntax/src/02-Values-and-Functions/02-Function-Currying.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Values-and-Functions/02-Function-Currying.purs
@@ -3,19 +3,33 @@ module Syntax.Function.Currying where
 import Prelude
 
 -- Remember this function?
-two_arg_function :: ParameterType1 -> ParameterType2 -> ReturnType
-two_arg_function argument1 argument2 = bodyThatReturnsType
+one_arg_function_syntax_sugar :: ParameterType -> ReturnType
+one_arg_function_syntax_sugar argument = bodyThatReturnsType
 
--- This is doing currying in the background. In reality, the function's
--- type signature is:
---    two_arg_function :: ParameterType1 -> (ParameterType2 -> ReturnType)
+-- it's syntax sugar for
+one_arg_function_no_syntax_sugar :: Function ParameterType ReturnType
+one_arg_function_no_syntax_sugar argument = bodyThatReturnsType
+
+-- Which means this function...
+two_arg_function0 :: ParameterType1 -> ParameterType2 -> ReturnType
+two_arg_function0 argument1 argument2 = bodyThatReturnsType
+
+-- is in a "curried" form. In reality, it's
+two_arg_function1 :: ParameterType1 -> (ParameterType2 -> ReturnType)
+two_arg_function1 argument1 argument2 = bodyThatReturnsType
+-- or without syntax sugar
+two_arg_function2 :: Function ParameterType1 (ParameterType2 -> ReturnType)
+two_arg_function2 argument1 argument2 = bodyThatReturnsType
+-- and removing the last one
+two_arg_function3 :: Function ParameterType1 (Function ParameterType2 ReturnType)
+two_arg_function3 argument1 argument2 = bodyThatReturnsType
 
 {-
 In other words, give me an argument (ParameterType1) and I'll return a
   function (ParameterType2 -> ReturnType). If you give that function
   a parameter (ParameterType2), then it'll give you the ReturnType.
 Since this happens in the background, we don't usually need to think about
-  it, but it is an important distinction to make. as it creates the following
+  it, but it is an important distinction to make as it creates the following
   Javascript code:
 
 two_arg_function = function (ParameterType1) -> {
