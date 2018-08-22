5f02f41438a6dc5ce0961330ee2f1be1f1d8561e
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-22 15:16:24 -0700

    Make example compil-able test; show other definition types

--- 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/01-Single-Paramter.purs
+++ 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/01-Single-Paramter.purs
@@ -5,12 +5,12 @@ import Prelude
 -- Basic Type classes
 
 -- a type class definition...
-class TypeClassName paramerType where
-  functionName :: paramerType -> ReturnType
+class TypeClassName parameterType where
+  functionName :: parameterType -> ReturnType
 
 -- Or the parameter type could be the return type:
-class TypeClassName_ paramerType where
-  fromString :: String -> paramerType
+class TypeClassName_ parameterType where
+  fromString :: String -> parameterType
 
 -- example
 class ToInt a where
@@ -24,8 +24,37 @@ instance toIntBoolean :: ToInt Boolean where
   toInt true = 1
   toInt false = 0
 
--- usage
--- (toInt true) == 0
+test :: Boolean
+test = (toInt true) == 0
+
+-- Type classes can also specify values:
+
+class TypeClassDefiningValue a where
+  value :: a
+
+instance intValue :: TypeClassDefiningValue Int where
+  value = 42
+
+-- Type classes usually only specify one function, but sometimes
+-- they specify multiple functions and/or values:
+
+class ZeroAppender a where
+  append :: a -> a -> a
+  zeroValue :: a
+
+instance intZeroAppender :: ZeroAppender Int where
+  append = (+)
+  zeroValue = 0
+
+{-
+Note: Type class instances that use type aliases (i.e. the `type` keyword)
+wil fail to compile. The following code demonstrates this.
+-}
+
+-- Uncomment me and I'll become a compiler error
+-- type Age = Int
+-- instance ageValue :: TypeClassDefiningValue Age where
+--   value = 2
 
 -- Type classes are useful for constraining types, which will be covered next.
 
