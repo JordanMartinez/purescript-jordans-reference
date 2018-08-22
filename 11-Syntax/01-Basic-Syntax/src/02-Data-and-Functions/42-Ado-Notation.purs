20ececbeba4ec3449408777d8e16c8ec622a9186
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-22 13:54:13 -0700

    Make ado notation compile-able

--- 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/42-Ado-Notation.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/42-Ado-Notation.purs
@@ -1,87 +1,84 @@
-module Syntax.Notation.Ado where
-
 {-
 WARNING! This syntax will only make sense if one understands Applicative
-from Prelude and the map/<$>, apply/<*>, and pure functions.
-Come back to this file once you understand that.
--}
+from Prelude and the `map`/`<$>`, `apply`/`<*>`, and `pure` functions.
+Come back to this file once you understand them.
 
-import Prelude -- hiding ((<$>), (<*>), Functor, Apply, Applicative)
+WARNING! This notation will currently cause the doc-generation to fail.
+See this issue for more details until the bug is fixed:
+https://github.com/purescript/purescript/issues/3414
+
+Link to original issue's comment
+where this is fully explained:
+https://github.com/purescript/purescript/pull/2889#issuecomment-301260299
+
+Following the 'do' notation of Monads, the 'ado' notation is for Applicative
+Since Applicative can be used for parellel computation, one **might**
+   read the following code as
+     "produces some value at the same time it's producing another value"
+   rather than sequential computation, which is
+     "produces some value, and then uses that value to produce another value"
+It depends on whether parallel applicatives are used or not.
+-}
+module Syntax.Notation.Ado where
+
+import Prelude
 
 data Box a = Box a
 
--- Copied from Prelude to give as reference:
-class Functor f where
-  map :: forall a b. f a -> (a -> b) -> f b
-
-class Apply f where
-  apply :: forall a b. f (a -> b) -> f a -> f b
-
-class Applicative f where
-  pure :: forall a. a -> f a
-
-infixl 4 map as <$>
-infixl 4 apply as <*>
-
 instance boxFunctor :: Functor Box where
-  map (Box a) f = Box (f a)
+  map :: forall a b. (a -> b) -> Box a -> Box b
+  map f (Box a) = Box (f a)
+-- infixl 4 map as <$>
 
 instance boxApply :: Apply Box where
+  apply :: forall a b. Box (a -> b) -> Box a -> Box b
   apply (Box f) (Box a) = Box (f a)
+-- infixl 4 apply as <*>
 
 instance boxApplicative :: Applicative Box where
+  pure :: forall a. a -> Box a
   pure a = Box a
+------------------------------
+pure_no_sugar :: forall a b. (a -> b) -> a -> Box b
+pure_no_sugar f a = pure (f a)
 
-{-
-WARNING!
+pure_sugar :: forall a b. (a -> b) -> a -> Box b
+pure_sugar f a = ado
+  in f a
+------------------------------
+map_no_sugar :: forall a b. (a -> b) -> Box a -> Box b
+map_no_sugar f g = (\x -> f x) <$> g
 
-This notation will cause the doc-generation to fail.
-See this issue for more details until the bug is fixed:
-https://github.com/purescript/purescript/issues/3414
--}
+map_sugar :: forall a b. (a -> b) -> Box a -> Box b
+map_sugar f g = ado
+  x <- g
+  in f x
+------------------------------
+-- See `lift2` from Apply: https://pursuit.purescript.org/packages/purescript-prelude/4.1.0/docs/Control.Apply#v:lift2
+liftN_no_sugar :: forall a b c. (a -> b -> c) -> Box a -> Box b -> Box c
+liftN_no_sugar f g h = (\x y -> f x y) <$> g <*> h
 
--- Link to original issue's comment
--- where this is fully explained: https://github.com/purescript/purescript/pull/2889#issuecomment-301260299
+liftN_sugar :: forall a b c. (a -> b -> c) -> Box a -> Box b -> Box c
+liftN_sugar f g h = ado
+  x <- g
+  y <- h
+  in f x y
+------------------------------
+liftN_unit_no_sugar :: forall a b. (a -> b) -> Box a -> Box Unit -> Box b
+liftN_unit_no_sugar f g h = (\x _ -> f x) <$> g <*> h
 
--- Following the 'do' notation of Monads, the 'ado' notation is for Applicative
--- Since Applicative can be used for parellel computation, one _might_
--- need to read the following code as
--- "produces some value at the same time it's producing another value"
--- rather than sequential computation, which is
--- "produces some value, and then uses that value to produce another value"
--- It depends on whether parallel applicatives are used or not.
+liftN_unit_sugar :: forall a b. (a -> b) -> Box a -> Box Unit -> Box b
+liftN_unit_sugar f g h = ado
+  x <- g
+  h
+  in f x
+------------------------------
+liftN_Let_no_sugar :: forall a. (Int -> Int -> a) -> Box Int -> Box Unit -> Box a
+liftN_Let_no_sugar f g h = (\x -> let y = x + 1 in (\_ -> f x y)) <$> g <*> h
 
-------------------------------
--- ado
---   in h x
--- -- desugars into
--- pure (h x)
-------------------------------
--- ado
---   x <- g
---   in f x
--- -- desugars into
--- (\x -> f x) <$> g
-------------------------------
--- ado
---   x <- g
---   y <- h
---   in f x y
--- -- desugars into
--- (\x y -> f x y) <$> g <*> h
-------------------------------
--- ado
---   x <- g
---   h
---   in f x
--- -- desugars into
--- (\x _ -> f x) <$> g <*> h
-------------------------------
--- ado
---   x <- g
---   let y = x + 1
---   h
---   in f x y
--- -- desugars into
--- (\x -> let y = x + 1 in \_ -> f x y) <$> g <*> h
-------------------------------
+liftN_Let_sugar :: forall a. (Int -> Int -> a) -> Box Int -> Box Unit -> Box a
+liftN_Let_sugar f g h = ado
+  x <- g
+  let y = x + 1
+  h
+  in f x y
