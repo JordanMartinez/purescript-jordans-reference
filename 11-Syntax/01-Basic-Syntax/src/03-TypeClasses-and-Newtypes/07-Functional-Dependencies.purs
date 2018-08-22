ff383754f482837073187891892d2fc93596e6c9
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-22 16:17:44 -0700

    Finish document-verifying FD syntax

--- 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/07-Functional-Dependencies.purs
+++ 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/07-Functional-Dependencies.purs
@@ -1,16 +1,23 @@
 module Syntax.Typeclass.MultiParameters.FunctionalDependencies where
 
-import Prelude
+{-
+This paper (section 2.1.2) quickly explains why we need functional dependencies:
+http://homepages.inf.ed.ac.uk/jmorri14/pubs/morris-icfp2010-instances.pdf
+In short, it prevents nonsensical code that is otherwise type-safe.
 
--- Sometimes in multi-parameter type classes, there is a relationship
--- between the types. In such cases, we call them 'functional dependencies'.
+Sometimes in multi-parameter type classes, there is a relationship
+between the types. In such cases, we call them 'functional dependencies'.
 
--- Syntax:
--- Read "type1 -> type2" as
---   'There is a function from type1 to type2 (e.g function :: type1 -> type2).'
--- or "'type1' determines what 'type2' will be"
+Syntax:
+  Read
+    "type1 -> type2"
+  as either
+    "There is a function from type1 to type2 (e.g function :: type1 -> type2)."
+  or
+    "'type1' determines what 'type2' will be"
+-}
 class TypeClassWithFunctionalDependency type1 type2 | type1 -> type2  where
-  functionName :: type1 -> type2
+  functionName1 :: type1 -> type2
 
 -- example (not sure whether this works...)
 
@@ -23,11 +30,12 @@ class Unwrap a b where
 instance unwrapBox :: Unwrap (Box a) a where
   unwrap (Box a) = a
 
--- TODO: Document 'type1 type2 -> type3' Functional dependency syntax
--- class TC type1 type2 type3 | type1 type2 -> type3 where
--- Functional dependency can go both ways:
-class BiDirectionFD type1 type2 | type1 -> type2, type2 -> type1 where
+------------------------
 
--- TODO: understand `purescript-generics-rep` library
--- link: https://pursuit.purescript.org/packages/purescript-generics-rep/6.0.0/docs/Data.Generic.Rep
--- repo: https://github.com/purescript/purescript-generics-rep
+-- If multiple types determine what another type is, use this syntax:
+class ManyTypesDetermineAnotherType a b c | a b {- n -} -> c  where
+  functionName2 :: a b -> c
+
+-- If multiple FDs exist, use this syntax:
+class ManyFDRelationships a b c | a {-n -} -> b, b -> c where
+  functionName3 :: a -> b -> c
