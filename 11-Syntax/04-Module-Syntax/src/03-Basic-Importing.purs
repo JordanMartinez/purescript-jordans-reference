5c32acccb74ae3e12949852141e78436fa69537d
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 13:57:29 -0700

    Re-export imports to remove compiler warnings

--- 11-Syntax/04-Module-Syntax/src/03-Basic-Importing.purs
+++ 11-Syntax/04-Module-Syntax/src/03-Basic-Importing.purs
@@ -1,37 +1,41 @@
-module Syntax.Module.Importing where
+-- For now, ignore the `module Exports` export
+-- and the "import Module (value) as Exports" syntax
+-- This will be explained later and is necessary now
+-- to prevent the compiler from emitting lots of warnings.
+module Syntax.Module.Importing (module Exports) where
 
--- imports just the module
-import Module
-
--- import a submodule
-import Module.SubModule.SubSubModule
+-- One never just imports the entire module.
+-- Rather, one must specify what is being imported.
+-- The following import statements emit a compiler warning:
+-- import Module
+-- import Module.SubModule.SubSubModule
 
 -- import values from a module
-import ModuleValues (value1, value2)
+import ModuleValues (value1, value2) as Exports
 
 -- imports functions from a module
-import ModuleFunctions (function1, function2)
+import ModuleFunctions (function1, function2) as Exports
 
 -- imports function alias from a module
-import ModuleFunctionAliases ((/=), (===), (>>**>>))
+import ModuleFunctionAliases ((/=), (===), (>>**>>)) as Exports
 
 -- imports type class from the module
-import ModuleTypeClass (class TypeClass)
+import ModuleTypeClass (class TypeClass) as Exports
 
 -- import a type but none of its constructors
-import ModuleDataType (DataType)
+import ModuleDataType (DataType) as Exports
 
 -- import a type and one of its constructors
-import ModuleDataType (DataType(Constructor1))
+import ModuleDataType (DataType(Constructor1)) as Exports
 
 -- import a type and some of its constructors
-import ModuleDataType (DataType(Constructor1, Constructor2))
+import ModuleDataType (DataType(Constructor1, Constructor2)) as Exports
 
 -- import a type and all of its constructors
-import ModuleDataType (DataType(..))
+import ModuleDataType (DataType(..)) as Exports
 
 -- import a kind and its instance
-import ModuleKind (kind ImportedKind, ImportedKindInstance)
+import ModuleKind (kind ImportedKind, ImportedKindInstance) as Exports
 
 -- To prevent warnings from being emitted during compilation
 -- the above imports have to either be used here or
