7f3d7be818d349d762c7b929434d4b0e83d14a76
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 11:48:32 -0700

    Improve readability; make examples compileable

--- 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/08-Functions-and-Data-with-Higher-Kinded-Types.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/08-Functions-and-Data-with-Higher-Kinded-Types.purs
@@ -5,10 +5,10 @@ import Prelude
 -- == Review ==
 data Box a = Box a
 -- `Box a` is a higher-kinded type (HKT). In other words,
--- it has a kind of `* -> *`, not `*` like Int. It still needs a type to be
--- specified before it is fully concrete.
+-- it has a kind of `Type -> Type`, not `Type` like Int.
+-- Its 'a' still needs to be specified before it is fully concrete.
 
--- we can define a function when the 'a' of Box is known (Int in this case)...
+-- we can define a function when the 'a' of Box is known ("Int" in this case)...
 add1 :: Box Int -> Box Int
 add1 (Box x) = Box (x + 1)
 
@@ -18,7 +18,7 @@ modify (Box a) function = Box (function a)
 
 -- However, how might one write a function that works on all of
 -- the four types below? In other words, how would we define a function
--- when we know we will have a higher-kinded type like `Box`,but
+-- when we know we will have a higher-kinded type like `Box`, but
 -- we don't know the exact type of that Box-like type?
 
 data Box1 a = Box1 a
@@ -29,16 +29,23 @@ data Box4 a = Box4 a
 -- The following function shows the syntax to follow.
 hktFunction0 :: forall f a. f a -> f a
 hktFunction0 boxN = boxN
--- One should understand `f a` as "f is a higher-kinded type that needs
--- one type, `a`, specified before it can be a concrete instance".
+{-
+Read
+  "f a"
+as
+  "f is a higher-kinded type
+    that needs one type, `a`, specified
+    before it can be a concrete instance"
 
--- When using higher-kinded types, convention is to start with `f` and continue
---   down the alphabet for each higher-kinded type thereafter
---   (e.g. `g`, `h`, etc.).
--- I think the convention of using 'f' has something to do with a typeclass
---   called Functor.
+When using higher-kinded types, convention is to start with `f` and continue
+down the alphabet for each higher-kinded type thereafter (e.g. `g`, `h`, etc.). -}
+
 hktFunction1 :: forall f g h a. f a -> g a -> h a -> h a
 hktFunction1 _ _ hOfA = hOfA
+
+-- I think the convention of using 'f' has something to do with a typeclass
+-- called Functor (covered in the Hello-World folder).
+
 
 -- If the higher-kinded type we want to include in our function takes more than
 -- one type, we just add the extra types beyond it
@@ -46,37 +53,62 @@ data HigherKindedTypeWith4Types a b c d = Constructor a b c d
 
 hktFunction2 :: forall f a b c d. f a b c d -> f a b c d
 hktFunction2 f_abcd = f_abcd
--- Read "f a b c d" as "F is a higher-kinded type that takes
--- 4 types, 'a', 'b', 'c', and 'd', all of which need to be specified
--- before 'f' can be a concrete type of kind `*`"
+{-
+Read the above
+  "f a b c d"
+as
+  "f is a higher-kinded type
+     that takes 4 types, 'a', 'b', 'c', and 'd',
+     all of which need to be specified
+     before 'f' can be a concrete type"
+-}
 
 -- We can also specify specific types in the function:
 hktFunction3 :: forall f a b c. f a b c Int -> f a b c Int
 hktFunction3 f_abc_Int = f_abc_Int
--- Read "f a b c Int" as "F is a higher-kinded type that takes
--- 4 types, 'a', 'b', 'c', and 'd'. 'd' has already been specified to 'Int',
--- but the others have yet to be specified. The compiler will complain if
--- one passes in an 'F' whose fourth type is not an Int.
+{-
+Read
+  "f a b c Int"
+as
+  "f is a higher-kinded type
+    that takes 4 types, 'a', 'b', 'c', and 'd'.
+
+  'd' has already been specified to 'Int',
+    but
+  the other types (a b c) have yet to be specified.
+
+  The compiler will complain
+    if one passes in an 'f' type whose fourth type is not an Int.
+-}
+
 
 -- Returning to our previous question...
 boxFunction :: forall f a. f a -> (f a -> a) -> (a -> a) -> (a -> f a) -> f a
 boxFunction boxN unwrap changeA rewrap =
   rewrap (changeA (unwrap boxN))
 -- The unwrap and rewrap functions in the above function are only needed to make
--- this compile. In many functions, they won't be needed due to typeclasses.
+-- this compile. In many functions, they won't be needed due to
+-- typeclasses (explained later).
 
 -- If we specified functions like below for each of the box type...
 unwrapBox2 :: forall a. Box2 a -> a
 unwrapBox2 (Box2 a) = a
 
+unwrapBox3 :: forall a. Box3 a -> a
+unwrapBox3 (Box3 a) = a
+
 rewrapBox2 :: forall a. a -> Box2 a
 rewrapBox2 a = Box2 a
 
-{-
-The following code would compile
-   (boxFunction (Box2 2) unwrapBox2 (_ + 1) rewrapBox2) == Box2 3
-   (boxFunction (Box3 3) unwrapBox3 (_ + 1) rewrapBox3) == Box3 4
--}
+rewrapBox3 :: forall a. a -> Box3 a
+rewrapBox3 a = Box3 a
+
+-- The following code will compile
+box2Example :: Box2 Int
+box2Example = boxFunction (Box2 2) unwrapBox2 (_ + 1) rewrapBox2 -- Box2 3
+
+box3Example :: Box3 Int
+box3Example = boxFunction (Box3 3) unwrapBox3 (_ + 1) rewrapBox3 -- Box3 4
 
 -- Keep in mind that any type that follows a 'forall' keyword could be
 -- a higher-kinded type
