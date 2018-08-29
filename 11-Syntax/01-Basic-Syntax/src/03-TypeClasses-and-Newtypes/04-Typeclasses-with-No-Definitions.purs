6f8036dccc0c7c17d4b0dcb83ead8fa64fb1721c
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 12:42:22 -0700

    Fix typo; improve explanation; note tc-combining class compiler quirk

--- 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/04-Typeclasses-with-No-Definitions.purs
+++ 11-Syntax/01-Basic-Syntax/src/03-TypeClasses-and-Newtypes/04-Typeclasses-with-No-Definitions.purs
@@ -7,7 +7,7 @@ Some type classes do not declare any functions or values in their definition.
 
 This usually occurs for one of two reasons:
 
-1. They are specify another law on top of the previous type class.
+1. They specify another law on top of the previous type class.
 
 For example, the `ToString` type class converts any instance into a String.
 -}
@@ -30,20 +30,27 @@ instance int50CharLimit :: ToString_50CharLimit Int -- no "where" keyword!
 {-
 A developer making a library that specifies such a type as this has
 documented through the types what users of that library should expect:
-calling 'toString a' will produce a String that is 50 chars or less.
+  calling 'toString a' will produce a String that is 50 chars or less.
 
-Note: Since `toString` is the method used here, even if one uses a function
-that includes a type constrained to `ToString`....
-    f :: forall a. (ToString a) => a -> String
-... it will still produce a String with 50 chars or less because the same
-method is being used for both `ToString` and `ToString_50CharLimit`.
+Note:
+Since the following are true...
+  - `toString` defines a function `toString`
+  - Int has an instance of `ToString`
+  - `ToString_50CharLimit` adds a law to `ToString`'s `toString` function
+  - Int has an instance of `ToString_50CharLimit`
+
+... then a function that uses `show Int` will still output a String
+that is 50 chars or less, even if the Int is not constrained to
+the `ToString_50CharLimit` type class.
+
 Since the latter imposes more restrictions on the former, the former must
 also abide by those restrictions.
-If one wanted to avoid this, they should specify a new function in
-`ToString_50CharLimit` that manipulates the `ToString`'s `toString` function:
 
-class (ToString a) <= ToString_50CharLimit a where
-  toString_limited a = to50OrLess $ show a
+If one wanted to avoid this, they should define a new type class that
+adds a function specifically for that:
+
+  class (ToString a) <= ToString_50CharLimt_2 a where
+    toString_limited a = -- implementaton that uses 'show'
 -}
 
 -- 2. Some type classes merely combine two or more type classes together:
@@ -59,14 +66,19 @@ class Unwrap a where
 class (Wrap a, Unwrap a) <= Boxable a
 
 -- To create an instance of `Boxable`, we need to define instances
--- for Wrap and Unwrap. Once both of those are satisfied, then
--- Boxable is also satisfied
+-- for Wrap and Unwrap. Usually, once both of those are satisfied, then
+-- Boxable is also satisfied.
 
 instance wrapInt :: Wrap Int where
   wrapIntoBox i = Box i
 
 instance unwrapInt :: Unwrap Int where
   unwrapFromBox (Box i) = i
+
+-- However, sometimes the compiler gets confused
+-- and thinks there isn't an instance for "Boxable". To be safe,
+-- define an instance for Boxable, even if it does nothing:
+instance boxableInt :: Boxable Int
 
 useBoxable :: forall a. (Boxable a) => a -> a
 useBoxable a = (unwrapFromBox <<< wrapIntoBox) a
