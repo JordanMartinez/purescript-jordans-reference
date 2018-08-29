fe23e1e6959deb0827ae5e79cd87c87021e5e2b6
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 12:05:21 -0700

    Rows: use clearer name for `binding` and functions

--- 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/09-Records/03-Row-Polymorphism.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/09-Records/03-Row-Polymorphism.purs
@@ -3,38 +3,44 @@ module Syntax.Record.RowPolymorphism where
 import Prelude
 
 -- We can also use literal records in our function type signatures:
-useName1 :: { name :: String } -> String
-useName1 { name: theName } = theName
+getName1 :: { name :: String } -> String
+getName1 { name: nameValue } = nameValue
 
-useName2 :: { name :: String } -> String
-useName2 person = person.name -- this syntax also works
-
-rowFunction :: { name :: String } -> String
-rowFunction {name: binding } = binding
+getName2 :: { name :: String } -> String
+getName2 person = person.name -- this syntax also works
 
 -- example
 test1 :: Boolean
 test1 =
-  (rowFunction { name: "hello" }) == "hello"
-  {- Compiler error: no other fields allowed!
-  rowFunction { name: "hello", age: 4 } == "hello" -}
+  (getName1 { name: "hello" }) == "hello"
+
+{-
+However, this definition does not prevent additional fields.
+The following code...
+
+  getName1 { name: "hello", age: 4 }
+
+...will output a compiler error since no other fields are allowed!
+-}
 
 
 -- There is a way to get rid of the compiler error using row polymorphism
-rowPolymorphism1 :: forall anyOtherFieldsThatMayExist. { name :: String | anyOtherFieldsThatMayExist } -> String
-rowPolymorphism1 { name: theName } = theName
+rowPolymorphism1 :: forall anyOtherFieldsThatMayExist
+                  . { name :: String | anyOtherFieldsThatMayExist }
+                 -> String
+rowPolymorphism1 { name: nameValue } = nameValue
 
 -- Rather than the "anyOtherFieldsThatMayExist" type name, convention is to
--- use "r". We can also use a "row.field" syntax to make things easier to read
--- but this will prevent pattern matching on the fields.
--- Rewriting our above function to use 'r' convention and the new syntax:
-rowPolymorphism2 :: forall r. { name :: String | r } -> String
-rowPolymorphism2 person = person.name
+-- use "r" for "rows". Rewriting our above function to use 'r' convention:
+getName4 :: forall r. { name :: String | r } -> String
+getName4 { name: nameValue } = nameValue
 
 -- examples
 test2 :: Boolean
 test2 =
-  (rowPolymorphism2 { name: "a name", age: 4, stuff: "?" }) == "a name" -- now it works!
+  (getName4 { name: "a name", age: 4, stuff: "?" }) == "a name" -- now it works!
 
--- Compiler errors arise when the required field doesn't exist, such as this example:
--- rowPolymorphism { age: 4, stuff: "?" }
+-- A compiler error will arise when the required field doesn't exist,
+-- such as this example:
+--
+--    getName4 { age: 4, stuff: "?" }
