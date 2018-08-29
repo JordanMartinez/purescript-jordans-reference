f0fa1f062188c244128ae2cf1ca45f1daafed36b
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 13:57:51 -0700

    Correct misunderstanding and remove unneeded explanation

--- 11-Syntax/04-Module-Syntax/src/05-Re-exporting-Modules-or-Submodules.purs
+++ 11-Syntax/04-Module-Syntax/src/05-Re-exporting-Modules-or-Submodules.purs
@@ -7,10 +7,6 @@ module Syntax.Module.ExportingModules
 -- (e.g. types, constructors, functions, values)
 -- from multiple modules conveniently
 
--- re-exports everything from Module1
--- Note: compiler will issue a warning if module is re-exported
--- without indicating what values/functions/types it imports
--- is not the same alias to re-export other modules
 import Module1 (anInt1) as M
 import Module2 (anInt2) as M
 import Module3 (anInt3) as M
