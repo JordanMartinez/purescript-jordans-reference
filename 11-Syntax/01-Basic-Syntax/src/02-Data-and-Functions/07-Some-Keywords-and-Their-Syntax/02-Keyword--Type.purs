8cba7a1acb10dd0cf229c25e38d33f82415a2490
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 11:28:19 -0700

    Provide example of alias inside type signature; refer to newtype for Age

--- 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/07-Some-Keywords-and-Their-Syntax/02-Keyword--Type.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/07-Some-Keywords-and-Their-Syntax/02-Keyword--Type.purs
@@ -3,15 +3,25 @@ module Keyword.Type where
 -- Syntax
 type TypeAliasForCompileTime = RunTimeType
 
--- examples
-type Age = Int
+-- Example
 type ComplexFunction = Int -> (forall a b. a -> (forall c. c-> b) -> b)
+
+-- and then use it here:
+-- someFunction :: String -> ComplexFunction -> ReturnType
+
+-- One could also do this...
+----------------------
+type Age = Int
 
 {-
 functionName :: ParamType1 -> ReturnType -}
 functionName :: Age        -> String
             -- 'Age' is a more descriptive type name than 'Int'
 functionName age = "body"
+
+----------------------
+-- ... but to do the above, one should use `newtype` instead,
+--   which is explained later.
 
 -- required to get this to compile correctly
 data RunTimeType
