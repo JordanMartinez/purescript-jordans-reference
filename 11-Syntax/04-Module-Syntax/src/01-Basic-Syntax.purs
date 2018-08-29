df803d3bf4a95ebaa673ec9c59e7f1f926289841
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-08-29 13:49:39 -0700

    Document exports; correct mistake: not conventions but compiler need

--- 11-Syntax/04-Module-Syntax/src/01-Basic-Syntax.purs
+++ 11-Syntax/04-Module-Syntax/src/01-Basic-Syntax.purs
@@ -1,12 +1,19 @@
-module Syntax.Module.Basic (exportedFunction) where
+module Syntax.Module.Basic
+  (
+  -- exports appear here
+  exportedFunction
+  ) where
 
--- imports (by convention) appear at the top
+-- imports must appear at the top or you'll get a compiler error
 import Prelude
 
--- everything else in the module (by convention) goes underneath it
+-- everything else in the module goes underneath it
 
 exportedFunction :: String -> String
 exportedFunction x = x <> "more stuff"
+
+-- an import cannot go here since we are no longer
+-- in the "import section" of the file
 
 notExportedValue :: Int
 notExportedValue = 3
