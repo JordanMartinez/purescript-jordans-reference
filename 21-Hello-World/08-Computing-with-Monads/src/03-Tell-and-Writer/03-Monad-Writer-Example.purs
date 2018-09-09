365833b073e2ae33c81f8b56031a67f607f9dd8a
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-09-08 17:34:49 -0700

    Write an overview of and examples for MonadTell / MonadWriter

---
+++ 21-Hello-World/08-Computing-with-Monads/src/03-Tell-and-Writer/03-Monad-Writer-Example.purs
@@ -0,0 +1,112 @@
+module ComputingWithMonads.Example.MonadWriter where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+import Data.Identity (Identity(..))
+import Data.Tuple (Tuple(..))
+import Control.Monad.Writer.Class (tell, listen, pass, listens, censor)
+import Control.Monad.Writer (Writer, runWriter)
+import Control.Monad.Writer.Trans (WriterT(..))
+
+main :: Effect Unit
+main = case runWriter writeStuff of
+  Tuple output otherUsefulData -> do
+    log $ "Computation's output: " <> show output
+    log $ "Other useful data:\n\n\t" <> otherUsefulData
+
+type Output = Int
+type OtherUsefulData = String
+
+                 --   w                 a
+           -- WriterT String Identity Int
+writeStuff :: Writer  String          Int
+writeStuff = do
+  (Tuple output1 usefulData) <- listen $ WriterT (
+        -- A computation that...
+        Identity (
+          -- returns this entire object as output,
+          -- so that one can use both the computed output
+          -- and the OtherUsefulData instance in later computations
+          Tuple
+            -- the output
+            1
+            -- the instance, which is appended via `tell` to
+            -- the current instance of OtherUsefulData
+            "useful data"
+        )
+    )
+  tell $ "\n\n\t" <> "(Listen) 'output1' was: " <> show output1
+  tell $ "\n\n\t" <> "(Listen) 'usefulData' was: " <> usefulData
+      -- needed to improve reading:
+      --  turns "usefulDatasomeData" into
+      -- "usefulData" <> "\n\n\t" <> "someData"
+      <> "\n\n\t"
+
+  Tuple two modifiedData <- listens
+
+    -- 2) ... a modified version of the non-output data
+    --          which is not appended via `tell`
+    --        before exposing it to the do notation
+    (\someData -> "Modified (" <> someData <> ")") $
+    WriterT (
+        -- 1) A computation...
+        Identity (
+          -- 2) that returns this entire object as output...,
+          -- so that one can use both the computed output and...
+          Tuple
+            -- the output
+            2
+            -- the instance, which is appended via `tell` to
+            -- the current instance of OtherUsefulData
+            "someData"
+        )
+    )
+  tell $ "\n\n\t" <> "(Listens) two: " <> show two
+  tell $ "\n\n\t" <> "(Listens) modified Data: " <> show modifiedData
+
+  output2 <- pass $ WriterT (
+      -- A computation...
+      Identity (
+
+        -- that returns 3 things using a nested Tuple
+        --    nested tuple: (Tuple (Tuple one three) two)
+        Tuple
+          (Tuple
+            -- 1) the computation's output
+            4
+            -- 3) a function which modifies the non-output data
+            --    before appending it to the current non-output data
+            --    instance
+            (\value -> "\n\n\t" <> "(in `pass`) Value is: " <> value)
+          )
+          -- 2) the non-output data
+          "value"
+        )
+    )
+
+  tell $ "\n\n\t" <> "(Pass) output2 was: " <> show output2
+    -- needed to improve reading:
+    --  turns "4someData" into
+    -- "4" <> "\n\n\t" <> "someData"
+    <> "\n\n\t"
+
+  output3 <- censor
+      -- 3) a function which modifies the non-output data
+      --    before appending it to the current non-output data
+      --    instance
+      (\value -> "(in `censor`) Value is: " <> value) $ WriterT (
+        -- 1) A computation...
+        Identity (
+          -- ...that returns two things
+          Tuple
+            -- a) the computation's output
+            2
+            -- b) the non-output data, which is...
+            "someData"
+        )
+      )
+
+  tell $ "\n\n\t" <> "(censor) output3 was: " <> show output3
+
+  pure 0
