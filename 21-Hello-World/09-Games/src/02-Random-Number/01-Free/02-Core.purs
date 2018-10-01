e2060541100084d35fc0b2b86c451904f186772d
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-10-01 13:26:21 -0700

    Move other game types to own file

--- 21-Hello-World/09-Games/src/02-Random-Number/01-Free/02-Core.purs
+++ 21-Hello-World/09-Games/src/02-Random-Number/01-Free/02-Core.purs
@@ -1,7 +1,5 @@
 module Games.RandomNumber.Free.Core
-  ( GameInfo(..), mkGameInfo
-  , GameResult(..)
-  , GameF(..)
+  ( GameF(..)
   , Game, game
   , module Exports
   ) where
@@ -23,28 +21,11 @@ import Games.RandomNumber.Free.Core.RemainingGuesses (
 , RemainingGuessesCreationError(..)
 ) as Exports
 
-type GameInfo = { bound :: Bounds
-                , number :: RandomInt
-                , remaining :: RemainingGuesses
-                }
+import Games.RandomNumber.Free.Core.GameTypes (GameInfo, GameResult)
+import Games.RandomNumber.Free.Core.GameTypes (
+  GameInfo, mkGameInfo, GameResult(..)
+) as Exports
 
--- | Convenience function for easily creating a GameInfo object
-mkGameInfo :: Bounds -> RandomInt -> RemainingGuesses -> GameInfo
-mkGameInfo bounds number remaining =
-  { bound: bounds, number: number, remaining: remaining }
-
--- | Sum type that defines the number of remaining guesses
--- | the player had before they won, or the random integer
--- | if they lost.
-data GameResult
-  = PlayerWins RemainingGuesses
-  | PlayerLoses RandomInt
-
-derive instance grE :: Eq GameResult
-
-instance grs :: Show GameResult where
-  show (PlayerWins rg) = "PlayerWins(" <> show rg <> ")"
-  show (PlayerLoses ri) = "PlayerLoses(" <> show ri <> ")"
 
 -- | High-level overview of our game's control flow
 data GameF a
