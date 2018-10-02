e6348912da81d8740125dc3affe89ec9c8644049
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-10-01 19:35:55 -0700

    Demonstrate remaining two issues with Free-based code via Run-based code

---
+++ 21-Hello-World/09-Games/src/02-Random-Number/12-Run/v2/01-Change-Implementation.purs
@@ -0,0 +1,100 @@
+module Games.RandomNumber.Run.VTwo.ChangeImplementation where
+
+import Prelude
+import Type.Row (type (+))
+import Data.Functor.Variant (on)
+import Run (Run, interpret, send, AFF, liftAff, runBaseAff)
+import Data.Either (Either(..))
+import Effect.Random (randomInt)
+import Effect.Class (liftEffect)
+import Effect (Effect)
+import Effect.Console (log)
+import Effect.Aff (Aff, runAff_, makeAff)
+import Node.ReadLine (
+  Interface
+, createConsoleInterface, noCompletion
+, close
+)
+import Node.ReadLine as NR
+
+import Games.RandomNumber.Core (GameResult(..), Bounds, Guess, mkGuess)
+import Games.RandomNumber.Run.VOne.Core (game)
+import Games.RandomNumber.Run.VOne.Domain (
+  runCore
+, NOTIFY_USER, notifyUser
+, DEFINE_BOUNDS, _defineBounds
+, DEFINE_TOTAL_GUESSES, _defineTotalGuesses
+, GEN_RANDOM_INT, _genRandomInt
+, MakeGuessF(..), MAKE_GUESS, _makeGuess)
+import Games.RandomNumber.Run.VOne.API (
+  runDomain
+, recursivelyRunUntilPure
+, GET_USER_INPUT, getUserInput, getIntFromUser
+, CREATE_RANDOM_INT
+, defineBoundsToAPI, defineTotalGuessesToAPI, genRandomIntToAPI
+)
+
+import Games.RandomNumber.Run.VOne.Infrastructure (runAPI)
+
+-- | Normally, the user would input their guess and cannot confirm whether
+-- | that is the user's final decision. In this interpretation, the user
+-- | must confirm their guess or the computer will ask for it again.
+makeAndConfirmGuess :: forall r. Bounds -> Run (NOTIFY_USER + GET_USER_INPUT + r) Guess
+makeAndConfirmGuess bounds = do
+  guess <- recursivelyRunUntilPure
+    ((mkGuess bounds) <$> getIntFromUser "Your guess: ")
+  notifyUser $ "You chose '" <> show guess <> "'"
+  confirmation <- getUserInput "Are you sure? ('y' = yes; anything else = no): "
+  case confirmation of
+    "y" -> pure guess
+    _ -> do
+      notifyUser "You changed your mind. I'll ask again."
+      makeAndConfirmGuess bounds
+
+makeGuessToAPI_2 :: forall r. MakeGuessF ~> Run (NOTIFY_USER + GET_USER_INPUT + r)
+makeGuessToAPI_2 (MakeGuessF bounds reply) = do
+  guess <- makeAndConfirmGuess bounds
+  pure (reply guess)
+
+runDomain_2 :: forall r
+           . Run (NOTIFY_USER +
+
+                  DEFINE_BOUNDS + DEFINE_TOTAL_GUESSES + GEN_RANDOM_INT +
+                  MAKE_GUESS +
+
+                  GET_USER_INPUT + CREATE_RANDOM_INT + r)
+          ~> Run (NOTIFY_USER +
+
+                  GET_USER_INPUT + CREATE_RANDOM_INT + r)
+runDomain_2 = interpret (
+  send
+    # on _defineBounds defineBoundsToAPI
+    # on _defineTotalGuesses defineTotalGuessesToAPI
+    # on _genRandomInt genRandomIntToAPI                              {-
+
+    Before:
+    # on _makeGuess makeGuessToAPI
+
+    After:                                                            -}
+    # on _makeGuess makeGuessToAPI_2
+  )
+
+main :: Effect Unit
+main = do
+  interface <- createConsoleInterface noCompletion
+
+  runAff_
+    (case _ of
+      Left _ -> close interface
+      Right gameResult -> case gameResult of
+        PlayerWins remaining -> do
+          log "Player won!"
+          log $ "Player guessed the random number with " <>
+            show remaining <> " trie(s) remaining."
+          close interface
+        PlayerLoses randomInt -> do
+          log "Player lost!"
+          log $ "The number was: " <> show randomInt
+          close interface
+    )
+    (runBaseAff $ runAPI interface (runDomain_2 (runCore game)))
---
+++ 21-Hello-World/09-Games/src/02-Random-Number/12-Run/v2/02-Add-Domain-Term.purs
@@ -0,0 +1,159 @@
+module Games.RandomNumber.Run.VTwo.AddDomainTerm where
+
+import Prelude
+import Data.Symbol (SProxy(..))
+import Data.Either (Either(..))
+import Effect (Effect)
+import Effect.Console (log)
+import Type.Row (type (+))
+import Data.Functor.Variant (on)
+import Node.ReadLine (
+  Interface
+, createConsoleInterface, noCompletion
+, close
+)
+import Effect.Aff (runAff_)
+import Run (Run, lift, FProxy(..), interpret, send, AFF, liftAff, runBaseAff)
+import Games.RandomNumber.Core ( Bounds
+                               , RandomInt, (==#)
+                               , RemainingGuesses, outOfGuesses, decrement
+                               , unRemainingGuesses
+                               , GameResult(..)
+                               )
+import Games.RandomNumber.Run.VOne.Core (
+  ExplainRulesF(..), _explainRules, EXPLAIN_RULES
+, SetupGameF(..), _setupGame, SETUP_GAME
+, PlayGameF(..), _playGame, PLAY_GAME
+, game
+)
+import Games.RandomNumber.Run.VOne.Domain (
+  runCore
+, NOTIFY_USER, notifyUser
+, DEFINE_BOUNDS, _defineBounds
+, DEFINE_TOTAL_GUESSES, _defineTotalGuesses
+, GEN_RANDOM_INT, _genRandomInt
+, MakeGuessF(..), MAKE_GUESS, _makeGuess, makeGuess
+, explainRulesToDomain, setupGameToDomain
+)
+import Games.RandomNumber.Run.VOne.API (
+  GET_USER_INPUT, _getUserInput, CREATE_RANDOM_INT, _createRandomInt
+, defineBoundsToAPI, defineTotalGuessesToAPI, makeGuessToAPI, genRandomIntToAPI)
+
+import Games.RandomNumber.Run.VOne.Infrastructure (runAPI)
+
+data TellJokeF a = TellJokeF a
+derive instance tjf :: Functor TellJokeF
+
+_tellJoke :: SProxy "tellJoke"
+_tellJoke = SProxy
+
+type TELL_JOKE r = (tellJoke :: FProxy TellJokeF | r)
+
+tellJoke :: forall r. Run (TELL_JOKE + r) Unit
+tellJoke = lift _tellJoke (TellJokeF unit)
+
+-- Algebras
+
+tellJokeToNotifyUser :: forall r. TellJokeF ~> Run (NOTIFY_USER + r)
+tellJokeToNotifyUser (TellJokeF next) = do
+  notifyUser "Why did the chicken cross the road?"
+  -- normally one would wait for the user's response here,
+  -- but I'm not going to do so to keep this example simple
+  notifyUser "To get to the other slide!"
+  notifyUser "What!? What do you mean that's not funny!? \
+             \...Well, what did you expect!? :-p"
+  pure next
+
+playGameToDomain_2 :: forall r
+                    . PlayGameF
+                   ~> Run (NOTIFY_USER + MAKE_GUESS + TELL_JOKE + r)
+playGameToDomain_2
+  (PlayGameF { bound: b, number: n, remaining: remaining } reply) = do
+    result <- gameLoop b n remaining
+
+    pure (reply result)
+
+-- Code
+
+gameLoop :: forall r
+          . Bounds -> RandomInt -> RemainingGuesses
+         -> Run (NOTIFY_USER + MAKE_GUESS + TELL_JOKE + r) GameResult
+gameLoop bounds randomInt remaining
+  | outOfGuesses remaining = pure $ PlayerLoses randomInt
+  | otherwise = do
+    let remaining' = decrement remaining
+    guess <- makeGuess bounds
+    if guess ==# randomInt
+      then pure $ PlayerWins remaining'
+      else do
+        notifyUser $ "Incorrect. You have " <> show remaining'
+          <> " guesses remaining."
+
+        -- new code! -----
+        when (unRemainingGuesses remaining' (_ == 1)) do
+          notifyUser "Oh no! Only 1 guess left!? How about I tell you \
+                     \a joke to help you calm down?"
+          tellJoke
+          notifyUser "That was a fun tangent! Back to the game!"
+        -------------
+
+        gameLoop bounds randomInt remaining'
+
+runCore_2 :: forall r
+         . Run (EXPLAIN_RULES + SETUP_GAME + PLAY_GAME +
+
+                -- new code ---
+                TELL_JOKE +
+                ---------------
+
+                NOTIFY_USER + DEFINE_BOUNDS + DEFINE_TOTAL_GUESSES +
+                GEN_RANDOM_INT + MAKE_GUESS + r)
+        ~> Run (TELL_JOKE +
+
+                NOTIFY_USER + DEFINE_BOUNDS + DEFINE_TOTAL_GUESSES +
+                GEN_RANDOM_INT + MAKE_GUESS + r)
+runCore_2 = interpret (
+  send
+    # on _explainRules explainRulesToDomain
+    # on _setupGame setupGameToDomain
+    # on _playGame playGameToDomain_2
+  )
+
+runDomain :: forall r
+           . Run (NOTIFY_USER + TELL_JOKE +
+
+                  DEFINE_BOUNDS + DEFINE_TOTAL_GUESSES + GEN_RANDOM_INT +
+                  MAKE_GUESS +
+
+                  GET_USER_INPUT + CREATE_RANDOM_INT + r)
+          ~> Run (NOTIFY_USER +
+
+                  GET_USER_INPUT + CREATE_RANDOM_INT + r)
+runDomain = interpret (
+  send
+    # on _defineBounds defineBoundsToAPI
+    # on _defineTotalGuesses defineTotalGuessesToAPI
+    # on _makeGuess makeGuessToAPI
+    # on _genRandomInt genRandomIntToAPI
+    # on _tellJoke tellJokeToNotifyUser
+  )
+
+main :: Effect Unit
+main = do
+  interface <- createConsoleInterface noCompletion
+
+  runAff_
+    (case _ of
+      Left _ -> close interface
+      Right gameResult -> case gameResult of
+        PlayerWins remaining -> do
+          log "Player won!"
+          log $ "Player guessed the random number with " <>
+            show remaining <> " trie(s) remaining."
+          close interface
+        PlayerLoses randomInt -> do
+          log "Player lost!"
+          log $ "The number was: " <> show randomInt
+          close interface
+    )
+    (runBaseAff $ runAPI interface (runDomain (runCore_2 game)))
