988f1d7f35630a46a452feab015f18e9ffb576fb
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-10-05 21:33:44 -0700

    Document Row syntax better

--- 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/09-Records/01-Basic-Syntax.purs
+++ 11-Syntax/01-Basic-Syntax/src/02-Data-and-Functions/09-Records/01-Basic-Syntax.purs
@@ -4,19 +4,32 @@ module Syntax.Record.Basic where
 
 -- "# Type" stands for "Row". It is a special kind used to indicate that
 -- there will be an N-sized number of types that are known at compile time.
+
+-- closed rows
+type Example_of_an_Empty_Row = ()
+type Example_of_a_Single_Row = (fieldName :: ValueType)
+type Example_of_a_Multiple_Rows = (first :: ValueType, second :: ValueType)
+
+-- open rows
+type Example_of_Open_Rows additionalRows =
+  (first :: ValueType, second :: ValueType | additionalRows)
+type Example_of_Anything_and_Everything_Rows r = ( | r)
+
+
 data Record_ -- # Type -> Type
 
 -- Think of records as a unordered named TupleN
-type RecordType = { field1 :: String
-               -- , ...
-                  , fieldN :: Int
-                  , function :: String -> String }
---    which desugars to
 type RecordType_Desugared = Record ( field1 :: String
                                 -- , ...
                                    , fieldN :: Int
                                    , function :: (String -> String)
-                                   ) -- "( fieldN :: TypeN )" is a Row
+                                   )
+-- However, there is syntax sugar for writing this:
+-- "Record ( rows )" becomes "{ rows }"
+type RecordType = { field1 :: String
+               -- , ...
+                  , fieldN :: Int
+                  , function :: String -> String }
 
 getField :: RecordType -> String
 getField obj = obj.field1
@@ -52,3 +65,6 @@ Syntax reminder:
 "field : value" - create the record's field
   { field: initialValue }
 -}
+
+-- needed to compile
+type ValueType = String
