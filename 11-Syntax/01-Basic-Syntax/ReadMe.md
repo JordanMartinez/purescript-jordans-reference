4bc2a0f5968be70e365fd9a45509bc16df104c13
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-10-10 11:37:28 -0700

    Split comments/docs file into separate files; show what docs look like

---
+++ 11-Syntax/01-Basic-Syntax/ReadMe.md
@@ -0,0 +1,12 @@
+# Basic Syntax
+
+Read through these files in their order. To further grasp the concept, write your own version of the code and see if it still compiles by running:
+```bash
+pulp --psc-package build
+```
+
+To see what the documentation looks like, run this command:
+```bash
+pulp --psc-package docs
+```
+Then, open the file, `./generated-docs/Syntax/Documentation.md`, to see what it outputs.
---
+++ 11-Syntax/01-Basic-Syntax/src/04-Documentation.purs
@@ -0,0 +1,58 @@
+-- | This is a single-line documentation.
+
+-- | This
+-- | is
+-- | a
+-- | multi-line
+-- | documentation block, not a comment.
+-- | Because it appears above the module declaration below,
+-- | it will be combined with the next few documentation blocks.
+
+-- | One can use markdown inside of documentation:
+-- |
+-- | Look an unordered list:
+-- | - item 1
+-- | - item 2
+-- |
+-- | An ordered list:
+-- | 1. Item
+-- | 2. Item
+-- | 3. Item
+-- |
+-- | A table:
+-- |
+-- | | One | Two | Three |
+-- | | - | - | - |
+-- | | a | b | c
+-- |
+-- | Some code:
+-- | ```purescript
+-- | f :: Int
+-- | f = 4
+-- | ```
+
+-- | Module-level documentation
+module Syntax.Documentation where
+
+-- | value documentation
+value :: Int
+value = 4
+
+-- | function documentation
+function :: Int -> String
+function _ = "easy"
+
+-- | data documentation
+data SomeData = SomeData
+
+-- | type documentation
+type MyType = String
+
+-- | newtype documentation
+newtype SmallInt = SmallInt Int
+
+-- | type class documentation
+class MyClass a b | a -> b
+
+-- | instance documentation
+instance example :: MyClass String Int
