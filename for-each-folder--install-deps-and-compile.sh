5d0e5c127c8482228fc3315bf81f702fbdc0e247
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-11-15 11:25:42 -0800

    Also remove old output, package sets, etc.

--- for-each-folder--install-deps-and-compile.sh
+++ for-each-folder--install-deps-and-compile.sh
@@ -11,22 +11,27 @@
 echo "... Syntax ...."
 
 cd 11-Syntax/01-Basic-Syntax/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 11-Syntax/02-Foreign-Function-Interface/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 11-Syntax/03-Type-Level-Programming-Syntax/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 11-Syntax/04-Module-Syntax/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 11-Syntax/05-Prelude-Syntax/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
@@ -35,14 +40,17 @@ cd ../../
 echo "... Hello World ...."
 
 cd 21-Hello-World/03-Hello-World-and-Effects/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 21-Hello-World/04-Debugging/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 21-Hello-World/05-Testing/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package test
 cd ../../
 
@@ -50,18 +58,22 @@ cd ../../
 # TODO: fix this by using Dhall
 #cd 21-Hello-World/06-Benchmarking/
 #
+#rm -rf .psc-package/ .pulp-cache/  output/
 #psc-package install && pulp --psc-package test
 #cd ../../
 
 cd 21-Hello-World/07-Type-Level-Programming/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 21-Hello-World/08-Application-Structure/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build
 cd ../../
 
 cd 21-Hello-World/09-Games/
+rm -rf .psc-package/ .pulp-cache/  output/
 psc-package install && pulp --psc-package build && pulp --psc-package test -m Test.Games.RandomNumber.Run.Infrastructure
 cd ../../
 
