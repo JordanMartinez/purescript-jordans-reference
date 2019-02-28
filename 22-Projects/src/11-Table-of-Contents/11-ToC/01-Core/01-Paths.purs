f0f34617be495acc27634488832e7a61f1c6780b
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-02-28 13:37:43 -0800

    Document core types

--- 22-Projects/src/11-Table-of-Contents/11-ToC/01-Core/01-Paths.purs
+++ 22-Projects/src/11-Table-of-Contents/11-ToC/01-Core/01-Paths.purs
@@ -9,18 +9,29 @@ module Projects.ToC.Core.Paths
 
 import Data.Semigroup ((<>))
 
+-- | Backend-independent indicator for whether a path is a directory or a file.
 data PathType
   = Dir
   | File
 
+-- | Backend-independent type for a path on the file system. It could be
+-- | "path" in "root/parent/path/child.txt", "file.txt" in
+-- | "root/parent/file.txt", or the entirety of "root/parent/file.txt"
 type FilePath = String
 
+-- | Backend-independent type for a website url. It could be
+-- | "https://github.com", "https://github.com/userName", or
+-- | "https://github.com/userName/projectName/blob/master/ReadMe.md"
 type WebUrl = String
 
+-- | Indicates the absolute path of either a directory or a file
+-- | in both 'path versions:' the file system and the website URL
 type UriPath = { fs :: FilePath
                , url :: WebUrl
                }
 
+-- | Adds the file path to file system's path using an os-specific file
+-- | separator character and to the current WebUrl
 type AddPath = UriPath -> FilePath -> UriPath
 
 -- | Creates an `AddPath` given a backend-specific way to get the file separator
--- 22-Projects/src/11-Table-of-Contents/11-ToC/01-Core/03-GitHubLinks.purs
+++ 22-Projects/src/11-Table-of-Contents/11-ToC/01-Core/03-GitHubLinks.purs
@@ -4,12 +4,17 @@ module Projects.ToC.Core.GitHubLinks
   ) where
 
 import Data.Semigroup ((<>))
+import Projects.ToC.Core.Paths (WebUrl)
 
+-- | All the info needed to render the URL to a file in the given
+-- | GitHub repository.
 type GitHubRepo = { username :: String
                   , repo :: String
                   , ref :: String
                   }
 
-renderGHPath :: GitHubRepo -> String
+-- | Produces a WebUrl that can be used as the url in UriPath for the
+-- | initial path.
+renderGHPath :: GitHubRepo -> WebUrl
 renderGHPath gh =
   "https://github.com/" <> gh.username <> "/" <> gh.repo <> "/blob/" <> gh.ref
