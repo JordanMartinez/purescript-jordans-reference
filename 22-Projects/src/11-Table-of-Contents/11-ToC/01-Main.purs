e0adb57a3ecce65783f3d261ffb3d7ad6d174de0
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-02-28 14:20:09 -0800

    Document ToC's Main file

--- 22-Projects/src/11-Table-of-Contents/11-ToC/01-Main.purs
+++ 22-Projects/src/11-Table-of-Contents/11-ToC/01-Main.purs
@@ -8,6 +8,8 @@ import Projects.ToC.API.AppM (runAppM)
 import Projects.ToC.Domain.BusinessLogic (program)
 import Projects.ToC.Infrastructure.CLI.Yargs (runProgramViaCLI)
 
+-- | Sets up the environment value via a CLI library
+-- | and then runs the program using that value.
 main :: Effect Unit
 main = do
   runProgramViaCLI (\env -> launchAff_ $ runAppM env program)
