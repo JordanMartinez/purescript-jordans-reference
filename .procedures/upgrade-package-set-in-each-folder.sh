f0f81318e4733f21d9337d3f4788403646a2b534
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-03-28 19:14:13 -0700

    Add helper file for update all folders' package sets to latest release

---
+++ .procedures/upgrade-package-set-in-each-folder.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# This file will update each folder's package set to the latest released one
+# It should be run at the root directory of this learning repository.
+
+echo "... Syntax ...."
+
+cd 11-Syntax/01-Basic-Syntax/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 11-Syntax/02-Foreign-Function-Interface/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 11-Syntax/03-Type-Level-Programming-Syntax/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 11-Syntax/04-Module-Syntax/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 11-Syntax/05-Prelude-Syntax/
+pwd
+spago package-set-upgrade
+cd ../../
+
+echo "... Hello World ...."
+
+cd 21-Hello-World/03-Hello-World-and-Effects/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 21-Hello-World/04-Debugging/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 21-Hello-World/05-Testing/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 21-Hello-World/06-Benchmarking/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 21-Hello-World/07-Type-Level-Programming/
+pwd
+spago package-set-upgrade
+cd ../../
+
+cd 21-Hello-World/08-Application-Structure/
+pwd
+spago package-set-upgrade
+cd ../../
+
+echo "... Projects ...."
+
+cd 22-Projects/
+pwd
+spago package-set-upgrade
+cd ../
+
+echo "... Finished ...."
