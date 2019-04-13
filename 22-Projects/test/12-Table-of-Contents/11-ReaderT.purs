29874cb7dfb06d3706eb9928eb3f471b27a74f82
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-04-12 18:12:33 -0700

    Finish implementing type classes for ToC tests

--- 22-Projects/test/12-Table-of-Contents/11-ReaderT.purs
+++ 22-Projects/test/12-Table-of-Contents/11-ReaderT.purs
@@ -1,21 +1,21 @@
-module Test.ToC.ReaderT where
+module Test.ToC.ReaderT (IncludedOrNot, VerifiedOrNot, FileSystemInfo(..), TestRows, TestEnv, TestState, TestM(..), runTestM, programAsTest, separator) where
 
 import Prelude
 
-import Control.Monad.Maybe.Trans (runMaybeT)
-import Control.Monad.Reader (class MonadAsk, ReaderT(..), ask, asks, lift, runReaderT)
-import Control.Monad.State (class MonadState, State, gets, put, runState)
-import Data.Array (last)
+import Control.Comonad.Cofree (head)
+import Control.Monad.Reader (class MonadAsk, ReaderT, asks, runReaderT)
+import Control.Monad.State (class MonadState, State, get, put, runState)
+import Data.Array (fold, foldl, last)
 import Data.List (List(..))
-import Data.Map (Map)
-import Data.Maybe (Maybe(..), fromJust, fromMaybe)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.String (Pattern(..), split)
 import Data.Tree (Tree)
-import Data.Tree.Zipper (children, findFromRoot, fromTree, value)
+import Data.Tree.Zipper (Loc, children, findFromRootWhere, fromTree, value)
+import Data.Tuple (Tuple)
 import Partial.Unsafe (unsafePartial)
-import TOC.Core.RenderTypes (TopLevelContent)
 import ToC.Core.FileTypes (HeaderInfo)
 import ToC.Core.Paths (FilePath, PathType(..), WebUrl)
+import ToC.Core.RenderTypes (TopLevelContent)
 import ToC.Domain.Types (Env)
 import ToC.ReaderT.Domain (class FileParser, class Logger, class ReadPath, class Renderer, class VerifyLink, class WriteToFile, program)
 import Type.Equality (class TypeEquals, from)
@@ -33,11 +33,11 @@ type TestEnv = Env TestRows
 
 type TestState = String
 
-newtype TestM a = TestM (ReaderT Env (State TestState) a)
+newtype TestM a = TestM (ReaderT TestEnv (State TestState) a)
 
-runTestM :: forall state a. state -> TestEnv -> TestM a -> a
-runTestM initialState env (TestM program) =
-  runState initialState (runReaderT program env)
+runTestM :: forall a. TestEnv -> TestM a -> Tuple a String
+runTestM env (TestM program) =
+  runState (runReaderT program env) ""
 
 programAsTest :: forall m.
                  Monad m =>
@@ -46,6 +46,8 @@ programAsTest :: forall m.
                  MonadAsk TestEnv m =>
                  Logger m =>
                  ReadPath m =>
+                 FileParser m =>
+                 Renderer m =>
                  VerifyLink m =>
 
                  -- Monad state that simulates impure code
@@ -61,21 +63,26 @@ programAsTest = do
   -- run the program, which will "write" its output into the state monad
   program
   -- get the final output
-  gets (\state -> state.content)
+  get
 
-instance monadAskTestM :: TypeEquals e Env => MonadAsk e TestM where
-  ask = TestM $ lift $ asks from
+instance monadAskTestM :: TypeEquals e TestEnv => MonadAsk e TestM where
+  ask = TestM $ asks from
 
 derive newtype instance functorTestM :: Functor TestM
 derive newtype instance applyTestM :: Apply TestM
 derive newtype instance applicativeTestM :: Applicative TestM
 derive newtype instance bindTestM :: Bind TestM
 derive newtype instance monadTestM :: Monad TestM
-derive newtype instance monadStateTestM :: MonadState state TestM
+derive newtype instance monadStateTestM :: MonadState String TestM
 
 -- helper functions
 separator :: String
 separator = "/"
+
+getPathName :: FileSystemInfo -> String
+getPathName = case _ of
+  DirectoryInfo name _ -> name
+  FileInfo name _ _ _ -> name
 
 -- implement type classes
 
@@ -85,35 +92,42 @@ instance logToConsoleTestM :: Logger TestM where
 instance readPathTestM :: ReadPath TestM where
   readDir :: FilePath -> TestM (Array FilePath)
   readDir path = do
-    fileSystemLoc <- asks \e -> fromTree e.fileSystem
-    maybeArrayPaths <- runMaybeT do
-      let pathSegments = split (Pattern separator) path
-      lastPath <- last pathSegments
-      foundLoc <- findFromRoot path fileSystemLoc
-      case value foundLoc of
-        DirectoryInfo _ _ -> pure $ (children foundLoc) <#> (\childLoc ->
-            case value childLoc of
-              DirectoryInfo path _ -> path
-              FileInfo path _ _ _ -> path
-          )
-        _ -> Nothing
+      fileSystemLoc <- asks \e -> fromTree e.fileSystem
+      let maybeArrayPaths = getChildren fileSystemLoc
 
-    -- catch possible bugs in our randomly-generated data
-    pure $ unsafePartial $ fromJust maybeArrayPaths
+      -- catch possible bugs in our randomly-generated data
+      pure $ unsafePartial $ fromJust maybeArrayPaths
+
+    where
+      getChildren :: Loc FileSystemInfo -> Maybe (Array FilePath)
+      getChildren fileSystemLoc = do
+        let pathSegments = split (Pattern separator) path
+        lastPath <- last pathSegments
+        foundLoc <- findFromRootWhere (\a -> getPathName a == lastPath) fileSystemLoc
+        case value foundLoc of
+          DirectoryInfo _ _ -> do
+            let pathList = children foundLoc <#> (\cofree -> getPathName $ head cofree)
+            let pathArray = foldl (\acc next -> acc <> [next]) [] pathList
+            pure pathArray
+          _ -> Nothing
 
   readFile :: FilePath -> TestM String
   readFile path = pure ""
 
   readPathType :: FilePath -> TestM (Maybe PathType)
   readPathType path = do
-    fileSystemLoc <- asks \e -> fromTree e.fileSystem
-    runMaybeT do
-      let pathSegments = split (Pattern separator) path
-      lastPath <- last pathSegments
-      foundLoc <- findFromRoot lastPath fileSystemLoc
-      pure $ case value foundLoc of
-        DirectoryInfo _ _ -> Dir
-        FileInfo _ _ _ _ -> File
+      fileSystemLoc <- asks \e -> fromTree e.fileSystem
+      pure $ getPathType fileSystemLoc
+
+    where
+      getPathType :: Loc FileSystemInfo -> Maybe PathType
+      getPathType fileSystemLoc = do
+        let pathSegments = split (Pattern separator) path
+        lastPath <- last pathSegments
+        foundLoc <- findFromRootWhere (\a -> getPathName a == lastPath) fileSystemLoc
+        pure $ case value foundLoc of
+          DirectoryInfo _ _ -> Dir
+          FileInfo _ _ _ _ -> File
 
 instance parseFileTestM :: FileParser TestM where
   parseFile :: FilePath -> String -> TestM (List (Tree HeaderInfo))
@@ -124,16 +138,16 @@ instance rendererTestM :: Renderer TestM where
   renderFile _ _ path _ = pure $ path <> "\n"
 
   renderDir :: Int -> FilePath -> Array String -> TestM String
-  renderDir _ path childrenContent = pure $ path <> "\n" <> childrenContent
+  renderDir _ path childrenContent = pure $ path <> "\n" <> fold childrenContent
 
   renderTopLevel :: FilePath -> Array String -> TestM TopLevelContent
   renderTopLevel path childrenContent =
-    pure $ { tocHeader: ""
-           , section: path <> "\n" <> childrenContent
-           }
+    pure { tocHeader: ""
+         , section: path <> "\n" <> fold childrenContent
+         }
 
   renderToC :: Array TopLevelContent -> TestM String
-  renderToC allContent = pure allContent.allSections
+  renderToC allContent = pure $ foldl (\acc next -> acc <> next.section) "" allContent
 
 instance writeToFileTestM :: WriteToFile TestM where
   writeToFile :: String -> TestM Unit
@@ -143,14 +157,17 @@ instance writeToFileTestM :: WriteToFile TestM where
 instance verifyLinksTestM :: VerifyLink TestM where
   verifyLink :: WebUrl -> TestM Boolean
   verifyLink url = do
-    fileSystemLoc <- asks \e -> fromTree e.fileSystem
-    maybeVerified <- runMaybeT do
-      let pathSegments = split (Pattern "/") url
-      lastPath <- last pathSegments
-      foundLoc <- findFromRoot lastPath fileSystemLoc
-      case foundLoc of
-        FileInfo _ _ _ verified -> Just verified
-        _ -> Nothing
+      fileSystemLoc <- asks \e -> fromTree e.fileSystem
+      let maybeVerified = getFileLink fileSystemLoc
 
-    -- catch possible bugs in our randomly-generated data
-    pure $ unsafePartial $ fromJust maybeVerified
+      -- catch possible bugs in our randomly-generated data
+      pure $ unsafePartial $ fromJust maybeVerified
+    where
+      getFileLink :: Loc FileSystemInfo -> Maybe Boolean
+      getFileLink fileSystemLoc = do
+        let pathSegments = split (Pattern "/") url
+        lastPath <- last pathSegments
+        foundLoc <- findFromRootWhere (\a -> getPathName a == lastPath) fileSystemLoc
+        case value foundLoc of
+          FileInfo _ _ _ verified -> Just verified
+          _ -> Nothing
