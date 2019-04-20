ba6d40bdd458c6419c1017baba040ba9a373271e
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-04-20 11:04:34 -0700

    Write data generators for markdown file's header parser

--- 22-Projects/test/12-Table-of-Contents/03-Parser-Logic/02-Generators.purs
+++ 22-Projects/test/12-Table-of-Contents/03-Parser-Logic/02-Generators.purs
@@ -1,5 +1,6 @@
 module Test.ToC.ParserLogic.Generators
   ( MarkdownParserResult(..)
+  , genMarkdownParserResult
   , PureScriptParserResult(..)
 
   ) where
@@ -10,19 +11,20 @@ import Control.Alt ((<|>))
 import Control.Comonad.Cofree (head, tail, (:<))
 import Control.Monad.Gen (chooseBool, oneOf)
 import Control.Monad.Rec.Class (Step(..), tailRecM)
-import Data.Array (cons, foldl)
-import Data.Array.ST.Iterator (next)
-import Data.Char.Gen (genAlpha, genDigitChar, genUnicodeChar)
+import Data.Array (cons, foldM)
+import Data.Char.Gen (genAlpha, genAsciiChar, genDigitChar, genUnicodeChar)
 import Data.Char.Unicode (isAlphaNum, isControl)
-import Data.Foldable (fold)
-import Data.List (List(..), filter, intercalate, (:))
+import Data.Foldable (fold, foldl)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.List (List(..), intercalate, reverse, (:))
 import Data.Monoid (power)
 import Data.NonEmpty ((:|))
 import Data.String.Gen (genString)
 import Data.Tree (Forest, Tree)
 import Data.Tuple (Tuple(..))
 import Test.QuickCheck (class Arbitrary)
-import Test.QuickCheck.Gen (Gen, chooseInt, frequency, listOf, suchThat, vectorOf, shuffle)
+import Test.QuickCheck.Gen (Gen, chooseInt, frequency, listOf, shuffle, suchThat, vectorOf)
 import ToC.Core.FileTypes (HeaderInfo)
 
 newtype MarkdownParserResult =
@@ -31,46 +33,67 @@ newtype MarkdownParserResult =
                        }
 
 instance arbitraryMarkdownParserResult :: Arbitrary MarkdownParserResult where
-  arbitrary = do
-    headerList <- genHeaderForest
-    lines <- genRenderedMarkdownLines headerList
-    pure $ MarkdownParserResult { expectedHeaders: headerList
-                                , lines: lines
-                                }
+  arbitrary = genMarkdownParserResult
+
+genMarkdownParserResult :: Gen MarkdownParserResult
+genMarkdownParserResult = do
+  headerForest <- genMarkdownHeaderForest
+  lines <- genRenderedMarkdownLines headerForest
+  pure $ MarkdownParserResult { expectedHeaders: headerForest
+                              , lines: lines
+                              }
 
 newtype PureScriptParserResult =
   PureScriptParserResult { expectedHeaders :: Forest HeaderInfo
                          , lines :: Array String
                          }
 
+-- TODO: The generators for this data type require keeping track of the
+-- number of lines that precede a given header
 instance arbitraryPureScriptParserResult :: Arbitrary PureScriptParserResult where
-  arbitrary = do
-    headerList <- genHeaderForest
-    lines <- genPureScriptLines headerList
-    pure $ PureScriptParserResult { expectedHeaders: headerList
-                                  , lines: lines
+  arbitrary = genPureScriptParserResult
+
+-- TODO: The generators for this data type require keeping track of the
+-- number of lines that precede a given header
+genPureScriptParserResult :: Gen PureScriptParserResult
+genPureScriptParserResult = do
+    pure $ PureScriptParserResult { expectedHeaders: Nil
+                                  , lines: []
                                   }
+
 
 data AnchorFragment
   = Word String
   | Hyphen
+  | UnderScore
   | WhiteSpace String
   | NonAnchorContent String
 
+derive instance genericAnchorFragment :: Generic AnchorFragment _
+
+instance showAnchorFragment :: Show AnchorFragment where
+  show x = genericShow x
+
+-- | Unboxes the AnchorFragment data constructor
 renderHeaderText :: AnchorFragment -> String
 renderHeaderText = case _ of
   Word x -> x
   Hyphen -> "-"
+  UnderScore -> "_"
   WhiteSpace x -> x
   NonAnchorContent x -> x
 
+-- | Unboxes the AnchorFragment data constructor except for NonAnchorContent,
+-- | which is not rendered
 renderAnchorText :: AnchorFragment -> String
 renderAnchorText = case _ of
   Word x -> x
   Hyphen -> "-"
+  UnderScore -> "_"
   WhiteSpace _ -> "-"
   NonAnchorContent _ -> ""
 
+-- | Generates a alphanumerical String
 genWordFragment :: Gen AnchorFragment
 genWordFragment = do
     totalChars <- chooseInt 1 5
@@ -79,13 +102,7 @@ genWordFragment = do
         oneOf $ genAlpha :| [genDigitChar]
     pure $ Word $ fold wordList
 
-genHyphen :: Gen AnchorFragment
-genHyphen = pure Hyphen
-
-genWhiteSpaceAnchorSeparator :: Gen AnchorFragment
-genWhiteSpaceAnchorSeparator = do
-  WhiteSpace <$> genWhitespace
-
+-- | Generates symbolic characters and non-alphanum non-whitespace characters
 genNonAnchorContent :: Gen AnchorFragment
 genNonAnchorContent = do
     totalChars <- chooseInt 1 4
@@ -98,60 +115,94 @@ genNonAnchorContent = do
         not ( isControl c
            || isAlphaNum c
            || c == ' '
+           || c == '\t'
            || c == '-'
+           || c == '_'
             )
       )
 
-genAnchorFragment :: Gen AnchorFragment
-genAnchorFragment =
+-- | Generates the non-whitespace characters
+genAnchorFragmentContent :: Gen AnchorFragment
+genAnchorFragmentContent =
   frequency $   Tuple 4.0 genWordFragment
-           :| ( Tuple 3.0 genNonAnchorContent
-              : Tuple 2.0 genWhiteSpaceAnchorSeparator
-              : Tuple 1.0 genHyphen
+           :| ( Tuple 2.0 genNonAnchorContent
+              : Tuple 2.0 (pure Hyphen)
+              : Tuple 1.0 (pure UnderScore)
               : Nil
               )
 
+-- | Generates a sequence of 1 or more spaces and/or tabs
 genWhitespace :: Gen String
 genWhitespace = do
   totalChars <- chooseInt 1 4
   fold <$> (listOf totalChars $ oneOf $ pure " " :| [pure "\t"])
 
-genHeaderInfo :: Int -> Gen HeaderInfo
-genHeaderInfo level = do
-    totalFragments <- chooseInt 1 6
-    startingFragment <- genWordFragment
-    remainingFragments <- vectorOf totalFragments genAnchorFragment
-    let fragments = startingFragment `cons` remainingFragments
-    let anchor = fold $ renderAnchorText <$> fragments
-    let headerText = fold $ renderHeaderText <$> fragments
+-- | Generates 1 header for a Markdown file
+genMarkdownHeaderInfo :: Int -> Gen HeaderInfo
+genMarkdownHeaderInfo level = do
+    -- generate all anchor fragments
+    guaranteeAtLeastOneWord <- genWordFragment
+    totalFragments <- chooseInt 0 6
+    allOtherFragments <- vectorOf totalFragments genAnchorFragmentContent
+    fragmentContents <- shuffle (guaranteeAtLeastOneWord `cons` allOtherFragments)
 
-    pure $ { anchor: "#" <> anchor, text: headerText, level: level }
+    -- add some whitespace between them
+    fragments <- intercalateGen (WhiteSpace <$> genWhitespace) fragmentContents
 
-genHeaderInfoTree :: Int -> Int -> Gen (Tree HeaderInfo)
-genHeaderInfoTree maxPossibleDepth headerLevel
+    -- create the corresponding anchor and text info
+    let rec = foldl (\acc next ->
+          { anchor: acc.anchor <> renderAnchorText next
+          , text: acc.text <> renderHeaderText next
+          }) { anchor: "", text: "" } fragments
+
+    pure { anchor: "#" <> rec.anchor, text: rec.text, level: level }
+  where
+    -- | Simulates 'intercalate whiteSpaceSeparation arrayOfGeneratedContent'
+    -- | by building up the list backwards (for speed)
+    -- | and then reversing the final output (for correct order)
+    intercalateGen :: Gen AnchorFragment -> Array AnchorFragment -> Gen (List AnchorFragment)
+    intercalateGen genSeparator foldable = do
+      result <- foldM (\acc next ->
+          if acc.init
+            then
+              pure { init: false, entity: next : acc.entity }
+            else do
+              separation <- genSeparator
+              pure $ acc { entity = next : separation : acc.entity }
+
+        ) { init: true, entity: Nil } foldable
+      pure $ reverse result.entity
+
+-- | Recursively generates a tree of markdown headers that may
+-- | or may not have children. However, the tree's depth is guaranteed
+-- | to be at least less than the max depth.
+genMarkdownHeaderInfoTree :: Int -> Int -> Gen (Tree HeaderInfo)
+genMarkdownHeaderInfoTree maxPossibleDepth headerLevel
   | maxPossibleDepth == headerLevel =
-      (\header -> header :< Nil) <$> (genHeaderInfo headerLevel)
+      (genMarkdownHeaderInfo headerLevel) <#> (\headerInfo -> headerInfo :< Nil)
   | otherwise = do
-      header <- genHeaderInfo headerLevel
+      header <- genMarkdownHeaderInfo headerLevel
       children <- ifM chooseBool
         (do
           amount <- chooseInt 1 4
-          listOf amount $ genHeaderInfoTree maxPossibleDepth (headerLevel + 1)
-          )
+          listOf amount $ genMarkdownHeaderInfoTree maxPossibleDepth (headerLevel + 1)
+        )
         (pure Nil)
       pure $ header :< children
 
-genHeaderForest :: Gen (Forest HeaderInfo)
-genHeaderForest = do
-  amount <- chooseInt 1 2
-  maxPossibleDepth <- chooseInt 1 2
-  listOf amount $ genHeaderInfoTree (maxPossibleDepth + 1) 2
+genMarkdownHeaderForest :: Gen (Forest HeaderInfo)
+genMarkdownHeaderForest = do
+  amount <- chooseInt 1 6
+  maxPossibleDepth <- chooseInt 1 4
+  -- since we only parse headers that are level 2 or higher
+  -- we increase `maxPossibleDepth` by one
+  listOf amount $ genMarkdownHeaderInfoTree (maxPossibleDepth + 1) 2
 
 genRenderedMarkdownLines :: Forest HeaderInfo -> Gen (Array String)
 genRenderedMarkdownLines forest =
   tailRecM renderForest { forest: forest, array: [] }
   where
-    renderForest :: { forest :: Forest HeaderInfo, array :: Array String }
+    renderForest ::           { forest :: Forest HeaderInfo, array :: Array String }
                  -> Gen (Step { forest :: Forest HeaderInfo, array :: Array String } (Array String))
     renderForest { forest: Nil, array: array } = pure $ Done array
     renderForest { forest: (head:remaining), array: array } = do
@@ -190,7 +241,7 @@ genLinesWithRandomContent = do
                , genLineOfGibberish
                ]
     let flattenedArray = join array_of_array
-    pure $ flattenedArray
+    pure flattenedArray
   where
     genBlankLine :: Gen (Array String)
     genBlankLine = pure [""]
@@ -206,7 +257,7 @@ genLinesWithRandomContent = do
     genGibberishWord = do
       numberOfChars <- chooseInt 1 15
       charList <- listOf numberOfChars $ genString
-        (genUnicodeChar `suchThat` \c ->
+        (genAsciiChar `suchThat` \c ->
           not ( isControl c
              || c == '#'
               )
@@ -227,6 +278,3 @@ genLinesWithRandomContent = do
 
     genLineOfGibberish :: Gen (Array String)
     genLineOfGibberish = vectorOf 1 genGibberishPhrase
-
-genPureScriptLines :: Forest HeaderInfo -> Gen (Array String)
-genPureScriptLines forest = pure []
