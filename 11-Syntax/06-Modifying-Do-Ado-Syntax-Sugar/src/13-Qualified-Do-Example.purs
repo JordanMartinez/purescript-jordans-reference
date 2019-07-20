981d21765cc7cb8f474de289ce224930f8f2e806
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-07-20 16:00:51 -0700

    Document qualified do/ado with working example

--- 11-Syntax/06-Modifying-Do-Ado-Syntax-Sugar/src/13-Qualified-Do-Example.purs
+++ 11-Syntax/06-Modifying-Do-Ado-Syntax-Sugar/src/13-Qualified-Do-Example.purs
@@ -1,4 +1,4 @@
-module Syntax.Notation.QualifiedDoAdo where
+module Syntax.QualifiedDo where
 
 -- we'll import Prelude so that the regular functions (e.g. "pure" "bind")
 -- are in scope to prove that they don't cause problems here.
@@ -7,25 +7,51 @@ import Prelude
 
 -- Requirement 3: import the module using a module alias, making it possible
 -- to use the same function names to refer to different "bind"-like functions
-import Syntax.Notation.MonadLikeTypeClasses as I
+import Syntax.MonadLikeTypeClasses as I
 
--- import the type classes, so we can constraint types
-import Syntax.Notation.MonadLikeTypeClasses (class IxMonad)
+-- import the type classes, so we can constrain types
+import Syntax.MonadLikeTypeClasses (class IxFunctor, class IxApply, class IxApplicative, class IxBind, class IxMonad)
 
--- Requirement 4: When we want to use 'qualified ado' syntax, we need to call the separate
--- function above and constrain the types to use IxMonad
-doExample :: forall f x. IxMonad f => f x x String
-doExample = I.do        -- signifies that we're using the "bind" function
-                        --   defined in the "MonadLikeTypeClasses" module
+-- Given a indexed-monadic type (type classes are at end of file)
+data Box phantomInput phantomOutput storedValue = Box storedValue
+
+-- Requirement 4: When we want to use 'qualified ado' syntax, we need to call
+-- the separate functions above and constrain the types to use IxMonad
+doExample :: forall input. Box input input String
+doExample = I.do        -- signifies that we're using the "bind" and "discard"
+                        --   functions defined in the "MonadLikeTypeClasses"
+                        --   module to desugar "<-" and lines that lack it
+                        --   (i.e. discard)
+
   a <- I.pure "test1"   -- signifies that we're using the "pure" function
                         --   defined in the "MonadLikeTypeClasses" module
   b <- I.pure "test2"
   I.pure (a <> b)
 
-mixingDosTogether :: String
-mixingDosTogether =
-  """
-  "Qualified do" and "unqualified do" can be mixed together. For examples, see
-    - https://github.com/spicydonuts/purescript-react-basic/blob/hooks-qualified-do/examples/counter/src/Counter.purs
-    - https://github.com/spicydonuts/purescript-react-basic/blob/hooks-qualified-do/examples/controlled-input/src/ControlledInput.purs
-  """
+instance ixFunctorBox :: IxFunctor Box where
+  imap :: forall a b x. (a -> b) -> Box x x a -> Box x x b
+  imap f (Box a) = Box (f a)
+
+instance ixApplyBox :: IxApply Box where
+  iapply :: forall a b x y z. Box x y (a -> b) -> Box y z a -> Box x z b
+  iapply (Box f) (Box a) = Box (f a)
+
+instance ixApplicativeBox :: IxApplicative Box where
+  ipure :: forall a x. a -> Box x x a
+  ipure a = Box a
+
+instance ixBindBox :: IxBind Box where
+  ibind :: forall a b x y z. Box x y a -> (a -> Box y z b) -> Box x z b
+  ibind (Box a) f =
+    -- `f a` produces a value with the type, `Box y z b`, which is
+    -- not the return type of this function, `Box x z b`.
+    --
+    -- So, we can either `unsafeCoerce` the result of `f a` or just
+    -- rewrap the 'b' value in a new Box. We've chosen to take the
+    -- latter option here for simplicity.
+    case f a of Box b -> Box b
+
+instance ixMonadBox :: IxMonad Box
+
+instance showBox :: (Show a) => Show (Box x x a) where
+  show (Box a) = "Box(" <> show a <> ")"
--- 11-Syntax/06-Modifying-Do-Ado-Syntax-Sugar/src/14-Qualified-Ado-Example.purs
+++ 11-Syntax/06-Modifying-Do-Ado-Syntax-Sugar/src/14-Qualified-Ado-Example.purs
@@ -1,4 +1,4 @@
-module Syntax.Notation.QualifiedAdoExample where
+module Syntax.QualifiedAdo where
 
 -- we'll import Prelude so that the regular functions (e.g. "map" "apply")
 -- are in scope to prove that they don't cause problems here.
@@ -7,21 +7,26 @@ import Prelude
 
 -- Requirement 3: import the module using a module alias, making it possible
 -- to use the same function names to refer to different "apply"-like functions
-import Syntax.Notation.MonadLikeTypeClasses as I
+import Syntax.MonadLikeTypeClasses as I
 
 -- import the type classes, so we can constraint types
-import Syntax.Notation.MonadLikeTypeClasses (class IxApplicative)
+import Syntax.MonadLikeTypeClasses (class IxFunctor, class IxApply, class IxApplicative)
+
+-- Given a indexed-monadic type (type classes are at end of file)
+data Box phantomInput phantomOutput storedValue = Box storedValue
 
 -- Requirement 4: When we want to use 'qualified ado' syntax, we need to call the separate
 -- function above and constrain the types to use IxApplicative
-adoExample :: forall f x. IxApplicative f => f x x String
-adoExample = I.ado       -- signifies that we're using the "apply" function
-                         --   defined in the "MonadLikeTypeClasses" module
+adoExample :: forall x. Box x x String
+adoExample = I.ado       -- signifies that we're using the "apply" and "map"
+                         --   functions defined in the "MonadLikeTypeClasses"
+                         --   module to desugar "<-" and "in <function>"
+                         --   notation.
+
   a <- I.pure "test1"    -- signifies that we're using the "pure" function
                          --   defined in the "MonadLikeTypeClasses" module
   b <- I.pure "test2"
-  in twoArgFunction a b  -- signifies that we're using the "map" function
-                         --   defined in the "MonadLikeTypeClasses" module
+  in twoArgFunction a b
 
 twoArgFunction :: String -> String -> String
 twoArgFunction a b = a <> b
@@ -32,3 +37,20 @@ mixingAdosTogether =
   I think "qualified ado" and "unqualified ado" can be mixed together,
   but I don't know of any examples
   """
+
+-- type class instances
+
+instance functor :: IxFunctor Box where
+  imap :: forall a b x. (a -> b) -> Box x x a -> Box x x b
+  imap f (Box a) = Box (f a)
+
+instance apply :: IxApply Box where
+  iapply :: forall a b x y z. Box x y (a -> b) -> Box y z a -> Box x z b
+  iapply (Box f) (Box a) = Box (f a)
+
+instance applicative :: IxApplicative Box where
+  ipure :: forall a x. a -> Box x x a
+  ipure a = Box a
+
+instance showBox :: (Show a) => Show (Box x x a) where
+  show (Box a) = "Box(" <> show a <> ")"
