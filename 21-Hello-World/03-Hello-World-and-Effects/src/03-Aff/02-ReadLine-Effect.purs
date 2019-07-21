3a67e3faab9e77e5d74833f12aecd8655c049875
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-07-21 00:44:12 -0700

    Cleanup Node.ReadLine Effect code and explanation

--- 21-Hello-World/03-Hello-World-and-Effects/src/03-Aff/02-ReadLine-Effect.purs
+++ 21-Hello-World/03-Hello-World-and-Effects/src/03-Aff/02-ReadLine-Effect.purs
@@ -1,4 +1,3 @@
-
 module ConsoleLessons.ReadLine.Effect where
 
 import Prelude
@@ -6,8 +5,8 @@ import Effect (Effect)
 import Effect.Console (log)
 
 {-
-This file will demonstrate why we have not been using Effect to
-teach our console-based lessons.
+This file will demonstrate why we can't use `Effect` to
+work with `Node.ReadLine`.
 
 Look through the code and then run it to see what happens.
 -}
@@ -41,7 +40,7 @@ main = do
   useInterface :: Interface -> Effect Unit
   useInterface interface = do
     log "Requesting user input..."
-    interface # question  "Type something here: "
+    interface # question "Type something here: "
         \answer -> log $ "You typed: '" <> answer <> "'\n"
 
   closeInterface :: Interface -> Effect Unit
@@ -73,17 +72,15 @@ In reality, it outputs this:
    [Program exit]
 
 The user never has a chance to type anything. Why?
-Because `question'`, and its underlying `question` function,
-adds a listener to the input stream that will run an action
-when the user has inputted some text and pressed Enter,
-and then continues evaluating the next statement.
-The next expression closes the interface,
+Because `question` adds a listener to the input stream
+that will run an action when the user has inputted some
+text and pressed Enter, and then continues evaluating
+the next statement. The next expression closes the interface,
 prevnting the user from ever inputting anything.
 
 In other words, it doesn't wait for the user to type in anything
 before continuing its evaluation.
 
-We got around this problem using the asychronous effect monad, Aff.
 Now it's time to see how we would write the same thing above
 using Aff.
 -}
