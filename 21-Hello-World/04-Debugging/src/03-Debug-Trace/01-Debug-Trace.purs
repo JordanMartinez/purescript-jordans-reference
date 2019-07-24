ecba6d5aa440b039d3166d8b54d2991a844a655f
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-07-23 18:12:16 -0700

    Clean up Debug.Trace overview

--- 21-Hello-World/04-Debugging/src/03-Debug-Trace/01-Debug-Trace.purs
+++ 21-Hello-World/04-Debugging/src/03-Debug-Trace/01-Debug-Trace.purs
@@ -15,14 +15,15 @@ import Debug.Trace (spy, trace, traceM)
 data Box a = Box a
 -- all type class instances are at the end of the file
 
-boxed4 :: Box Int
-boxed4 = Box 4
-
+-- And a way to convert a Box computation into an Effect computation
 runBox :: Box ~> Effect
 runBox (Box a) = pure a
 
+boxed4 :: Box Int
+boxed4 = Box 4
+
 printAndReturnTheValue :: Int -> Int
-printAndReturnTheValue x = spy "'x' is" x
+printAndReturnTheValue x = spy "x" x
 
 printMessageThenRunComputation :: String -> (Unit -> Int) -> Int
 printMessageThenRunComputation msg x = trace msg x
@@ -33,6 +34,9 @@ main :: Effect Unit
 main = do
   -- spy
   log $ show $ printAndReturnTheValue 5
+
+  -- another way to do this
+  let _ = spy "four" 4
 
   -- trace
   log $ show $ printMessageThenRunComputation "before computation" (\_ -> 10)
@@ -47,9 +51,6 @@ main = do
     -- it returns `Effect Unit`, not `Box Unit`
 
     -- Instead, we'll use traceM
-
-    -- Note: Using "traceM" without there being a monad instance
-    -- for Box causes this code to fail to compile. I'm not sure why...
     traceM ("Four is: " <> show four)
 
     pure (four + 8)
@@ -57,8 +58,6 @@ main = do
   log $ "Value is: " <> show value
 
 -- Box's type class instances
-instance monadBox :: Monad Box
-
 instance functorBox :: Functor Box where
   map f (Box a) = Box (f a)
 
@@ -70,3 +69,5 @@ instance applicativeBox :: Applicative Box where
 
 instance bindBox :: Bind Box where
   bind (Box a) f = f a
+
+instance monadBox :: Monad Box
