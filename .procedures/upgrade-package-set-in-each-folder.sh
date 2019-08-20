f4782308f52da10e22ebd6cf2a79b2591feb0c00
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-08-19 17:33:45 -0700

    Update package-set upgrading command

--- .procedures/upgrade-package-set-in-each-folder.sh
+++ .procedures/upgrade-package-set-in-each-folder.sh
@@ -7,71 +7,71 @@ echo "... Syntax ...."
 
 cd 11-Syntax/01-Basic-Syntax/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 11-Syntax/02-Foreign-Function-Interface/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 11-Syntax/03-Type-Level-Programming-Syntax/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 11-Syntax/04-Module-Syntax/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 11-Syntax/05-Prelude-Syntax/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 11-Syntax/06-Modifying-Do-Ado-Syntax-Sugar/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 echo "... Hello World ...."
 
 cd 21-Hello-World/03-Effect-and-Aff/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 21-Hello-World/04-Debugging/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 21-Hello-World/05-Application-Structure/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 21-Hello-World/06-Type-Level-Programming/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 21-Hello-World/07-Testing/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 cd 21-Hello-World/08-Benchmarking/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../../
 
 echo "... Projects ...."
 
 cd 22-Projects/
 pwd
-spago package-set-upgrade
+spago upgrade-set
 cd ../
 
 echo "... Finished ...."
