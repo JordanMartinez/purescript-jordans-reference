a9d27a15ed84ccd80e290c7bd2b89cc8d1230b54
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-09-07 20:31:16 -0700

    Reposition end of line FFI to Core

--- 22-Projects/13-Mdbook-ToC/src/01-Core/01-EOL.purs
+++ 22-Projects/13-Mdbook-ToC/src/01-Core/01-EOL.purs
@@ -1,5 +1,5 @@
 -- | FFI for Node's platform independent end-of-line String value.
-module ToC.Infrastructure.OSFFI (endOfLine) where
+module ToC.Core.EOL where
 
 -- | This is the 'String' version of `purescript-node-os`'s `eol` value,
 -- | a `Char` type.
