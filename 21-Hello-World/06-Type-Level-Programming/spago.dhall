bd3011f45767659cfdd426e9d81f395493116d43
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-06-06 07:07:14 -0700

    Explain how RowList works

--- 21-Hello-World/06-Type-Level-Programming/spago.dhall
+++ 21-Hello-World/06-Type-Level-Programming/spago.dhall
@@ -2,18 +2,17 @@
 Welcome to a Spago project!
 You can edit this file as you like.
 -}
-{ sources =
-    [ "src/**/*.purs" ]
-, name =
-    "untitled"
+{ sources = [ "src/**/*.purs" ]
+, name = "untitled"
 , dependencies =
-    [ "console"
-    , "effect"
-    , "prelude"
-    , "psci-support"
-    , "tuples"
-    , "typelevel-prelude"
-    ]
-, packages =
-    ../../packages.dhall
+  [ "arrays"
+  , "console"
+  , "effect"
+  , "lists"
+  , "prelude"
+  , "psci-support"
+  , "tuples"
+  , "typelevel-prelude"
+  ]
+, packages = ../../packages.dhall
 }
---
+++ 21-Hello-World/06-Type-Level-Programming/src/05-RowList/02-Advanced.purs
@@ -0,0 +1,63 @@
+module TLP.RowList.Advanced where
+
+import Prelude
+
+import Prim.Row as Row
+import Prim.RowList as RL
+import Prim.Symbol (class Append)
+
+                                                                              {-
+Now that we understand how `RowList`s work, let's use them to
+map row kinds. In other words, given some input rows, produce
+some output rows that have been modified slightly.                            -}
+
+-- Given an input row (and the rowlist produced by `RL.RowToList inputrows rowList`)
+-- produce an output row where each label has an "x" appended to it.
+class MapRowsAppendXToLabel :: RL.RowList Type -> Row Type -> Row Type -> Constraint
+class MapRowsAppendXToLabel rowList inputRows outputRows | rowList inputRows -> outputRows
+
+instance MapRowsAppendXToLabel RL.Nil emptyRow emptyRow
+instance (
+  -- 1. Use Row.Cons to get the rest of the input rows
+  Row.Cons sym a remainingInputRows inputRows,
+  -- 2. Append an "x" to the original label
+  Append sym "x" symWithXAppended,
+  -- 3. Use Row.Cons to update the current label for the output rows.
+  Row.Cons symWithXAppended a remainingOutputRows outputRows,
+  -- 4. Recursively compute the rest of the rows
+  MapRowsAppendXToLabel remainingRowList remainingInputRows remainingOutputRows
+  ) => MapRowsAppendXToLabel (RL.Cons sym a remainingRowList) inputRows outputRows
+
+-- | Proves that the 'updated' record is the same as 'original'
+-- | record but has an 'x' appended to each label
+proof
+  :: forall originalRowList original updated
+   . RL.RowToList original originalRowList
+  => MapRowsAppendXToLabel originalRowList original updated
+  => { | original }
+  -> { | updated }
+  -> Unit
+proof _ _ = unit
+                                                                            {-
+spago repl
+proof1
+proof2                                                                      -}
+
+proof1 :: Unit
+proof1 =
+  proof
+    { a : 4, b : "", c : false }
+    { ax: 4, bx: "", cx: false }
+
+proof2 :: Unit
+proof2 =
+  proof
+    { rowlist : 4, is : "", cool : false }
+    { rowlistx: 4, isx: "", coolx: false }
+
+-- This will fail to compile
+-- proof3 :: Unit
+-- proof3 =
+--   proof
+--     { rowlist : 4, is : "", cool : false }
+--     { rowlist : 4, is : "", cool : false }
