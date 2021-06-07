19d9981f0346eec7c558b678226dc34a3123cffa
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-06-07 07:01:37 -0700

    Remove old comments used to prepare for breaking change migration

--- 11-Syntax/04-Module-Syntax/src/modules-used-in-examples/ModuleKind.purs
+++ 11-Syntax/04-Module-Syntax/src/modules-used-in-examples/ModuleKind.purs
@@ -1,12 +1,5 @@
--- PureScript 0.13.x
 module ModuleKind (ImportedKind, ImportedKindValue) where
 
--- PureScript 0.14.x
--- module ModuleKind (ImportedKind, ImportedKindValue) where
-
--- PureScript 0.13.x
 data ImportedKind
--- PureScript 0.14.x
--- data ImportedKind
 
 foreign import data ImportedKindValue :: ImportedKind
