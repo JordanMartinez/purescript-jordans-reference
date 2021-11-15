59ea1991f6604190ac923039917dfab646367f10
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-11-14 21:32:55 -0800

    Removed redundant statement

--- .ci/install-build-test-all.sh
+++ .ci/install-build-test-all.sh
@@ -2,10 +2,6 @@
 
 # This script performs all the steps to install, build, and test
 # the example programs for this project.
-#
-# Run this from the root of this project:
-#
-#   source .ci/install-build-test-all.sh
 
 source .ci/spago--install.sh
 source .ci/spago--build-and-test.sh
