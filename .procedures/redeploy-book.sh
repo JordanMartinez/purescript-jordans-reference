e3405fe1c4b963c588acf0b17e62ea152b7f0803
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-12-12 16:44:38 -0600

    Update redeploy instructions

--- .procedures/redeploy-book.sh
+++ .procedures/redeploy-book.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 # This file assumes the following things are true:
-# - `./procedures/mdbook-toc.js` has been built via the `Projects/Mdbook-ToC`
+# - `./procedures/mdbook-toc.mjs` has been built (see https://github.com/JordanMartinez/purescript-mdbook-generator)
 #       project.
 # - `../purescript-jordans-reference-site` is a git repo and exists
 # - `../purescript-jordans-reference-site/src` folder exists
 # - `../purescript-jordans-reference-site/src/Summary-header.md` file exists
+# - `mdbook` and `mdbook-mermaid` have been installed and are on your PATH: 
+#    - https://github.com/rust-lang/mdBook/tree/master
+#    - https://github.com/badboy/mdbook-mermaid
 
 # Regenerate `mdbook`'s source content
-node .procedures/mdbook-toc.js -r "." -o "../purescript-jordans-reference-site/src" -s "Summary-header.md" -t ".ci,.git,.github,.procedures,.travis,output,book,mdbook,old-changelogs"
+node .procedures/mdbook-toc.mjs -r "." -o "../purescript-jordans-reference-site/src" -s "Summary-header.md" -t ".ci,.git,.github,.procedures,.travis,output,book,mdbook,old-changelogs,CHANGELOG.d"
 
 cd ../purescript-jordans-reference-site
 
